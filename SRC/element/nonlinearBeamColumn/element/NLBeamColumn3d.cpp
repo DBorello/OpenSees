b6f22930ec21143ec56c5c2c546b711c249edd01
jeremic <jeremic@0eff8f49-d3cf-49fd-9ff1-3c36910be21e>
2001-06-23 06:01:09 +0000

    Boris Jeremic (@ucdavis.edu): Similar to the previous change for  NLBeamColumn2d.cpp: Line 1133 not portable! Order of evaluation is undefined! C++ standard: "The order of evaluation of arguments                [of a function call] is unspecified." 														 [5.2.2 paragraph 9] Frank and Mike: please check this one as well two new lines created and check where "loc" should be incremented (++).
    git-svn-id: svn://peera.berkeley.edu/usr/local/svn/OpenSees/trunk@306 0eff8f49-d3cf-49fd-9ff1-3c36910be21e

--- SRC/element/nonlinearBeamColumn/element/NLBeamColumn3d.cpp
+++ SRC/element/nonlinearBeamColumn/element/NLBeamColumn3d.cpp
@@ -19,8 +19,8 @@
 **                                                                    **
 ** ****************************************************************** */
                                                                         
-// $Revision: 1.5 $
-// $Date: 2001-03-29 03:56:11 $
+// $Revision: 1.6 $
+// $Date: 2001-06-23 06:01:09 $
 // $Source: /usr/local/cvs/OpenSees/SRC/element/nonlinearBeamColumn/element/NLBeamColumn3d.cpp,v $
                                                                         
                                                                         
@@ -1130,7 +1130,8 @@ NLBeamColumn3d::recvSelf(int commitTag, Channel &theChannel, FEM_ObjectBroker &t
   // put  distrLoadCommit into the Vector
   for (i=0; i<NL; i++) 
   {
-    dData(loc) = distrLoadcommit(i) = dData(loc++);
+    distrLoadcommit(i) = dData(loc++);
+    dData(loc) = distrLoadcommit(i);
     loc++;
   }
 
