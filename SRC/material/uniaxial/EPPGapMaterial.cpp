67324457d3ba5ccefb1c8131eb482fc2a96be917
mhscott <mhscott@0eff8f49-d3cf-49fd-9ff1-3c36910be21e>
2001-07-10 16:12:09 +0000

    mhscott -- Modified to return elastic tangent when gap is open
    git-svn-id: svn://peera.berkeley.edu/usr/local/svn/OpenSees/trunk@310 0eff8f49-d3cf-49fd-9ff1-3c36910be21e

--- SRC/material/uniaxial/EPPGapMaterial.cpp
+++ SRC/material/uniaxial/EPPGapMaterial.cpp
@@ -18,8 +18,8 @@
 **                                                                    **
 ** ****************************************************************** */
                                                                         
-// $Revision: 1.1.1.1 $
-// $Date: 2000-09-15 08:23:22 $
+// $Revision: 1.2 $
+// $Date: 2001-07-10 16:12:09 $
 // $Source: /usr/local/cvs/OpenSees/SRC/material/uniaxial/EPPGapMaterial.cpp,v $
 
 // File: ~/material/EPPGapMaterial.C
@@ -114,17 +114,17 @@ double
 EPPGapMaterial::getTangent(void)
 {
     if (fy >= 0) {
-       if (trialStrain >= maxElasticYieldStrain)
+       if (trialStrain > maxElasticYieldStrain)
            return 0;
-       else if (trialStrain <= minElasticYieldStrain)
+       else if (trialStrain < minElasticYieldStrain)
            return 0;
        else
            return E;
     }
     else {
-       if (trialStrain <= maxElasticYieldStrain)
+       if (trialStrain < maxElasticYieldStrain)
            return 0;
-       else if (trialStrain >= minElasticYieldStrain)
+       else if (trialStrain > minElasticYieldStrain)
            return 0;
        else
            return E;
