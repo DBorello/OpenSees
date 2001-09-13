72bf0e7c3da12820a9bc47a928f5d185f833ef3d
jeremic <jeremic@0eff8f49-d3cf-49fd-9ff1-3c36910be21e>
2001-09-13 19:12:16 +0000

    small changes, mostly in comments... Boris Jeremic@ucdavis.edu
    git-svn-id: svn://peera.berkeley.edu/usr/local/svn/OpenSees/trunk@604 0eff8f49-d3cf-49fd-9ff1-3c36910be21e

--- SRC/nDarray/nDarray.cpp
+++ SRC/nDarray/nDarray.cpp
@@ -1,25 +1,5 @@
-/* ****************************************************************** **
-**    OpenSees - Open System for Earthquake Engineering Simulation    **
-**          Pacific Earthquake Engineering Research Center            **
-**                                                                    **
-**                                                                    **
-** (C) Copyright 1999, The Regents of the University of California    **
-** All Rights Reserved.                                               **
-**                                                                    **
-** Commercial use of this program without express permission of the   **
-** University of California, Berkeley, is strictly prohibited.  See   **
-** file 'COPYRIGHT'  in main directory for information on usage and   **
-** redistribution,  and for a DISCLAIMER OF ALL WARRANTIES.           **
-**                                                                    **
-** Developed by:                                                      **
-**   Frank McKenna (fmckenna@ce.berkeley.edu)                         **
-**   Gregory L. Fenves (fenves@ce.berkeley.edu)                       **
-**   Filip C. Filippou (filippou@ce.berkeley.edu)                     **
-**                                                                    **
-** ****************************************************************** */
-                                                                        
-// $Revision: 1.1 $
-// $Date: 2001-08-23 16:45:47 $
+// $Revision: 1.2 $
+// $Date: 2001-09-13 19:12:16 $
 // $Source: /usr/local/cvs/OpenSees/SRC/nDarray/nDarray.cpp,v $
                                                                         
                                                                         
