af0d37f0a0d16f66e0a71e29b4371f6ec64eb22f
mhscott <mhscott@0eff8f49-d3cf-49fd-9ff1-3c36910be21e>
2001-11-19 23:00:56 +0000

    Adding LAPACK subroutines for standard eigenvalue analysis
    git-svn-id: svn://peera.berkeley.edu/usr/local/svn/OpenSees/trunk@711 0eff8f49-d3cf-49fd-9ff1-3c36910be21e

--- OTHER/LAPACK/dlarnv.f
+++ OTHER/LAPACK/dlarnv.f
@@ -1,9 +1,9 @@
       SUBROUTINE DLARNV( IDIST, ISEED, N, X )
 *
-*  -- LAPACK auxiliary routine (version 2.0) --
+*  -- LAPACK auxiliary routine (version 3.0) --
 *     Univ. of Tennessee, Univ. of California Berkeley, NAG Ltd.,
 *     Courant Institute, Argonne National Lab, and Rice University
-*     September 30, 1994 
+*     September 30, 1994
 *
 *     .. Scalar Arguments ..
       INTEGER            IDIST, N
