79ecce9bc4b99b0c4c6a6cea0280162c2e764bb8
jeremic <jeremic@0eff8f49-d3cf-49fd-9ff1-3c36910be21e>
2002-01-06 19:41:50 +0000

    small changes -> standardization. Boris Jeremic@ucdavis.edu
    git-svn-id: svn://peera.berkeley.edu/usr/local/svn/OpenSees/trunk@759 0eff8f49-d3cf-49fd-9ff1-3c36910be21e

--- SRC/element/nonlinearBeamColumn/element/NLBeamColumn2d.cpp
+++ SRC/element/nonlinearBeamColumn/element/NLBeamColumn2d.cpp
@@ -18,8 +18,8 @@
 **                                                                    **
 ** ****************************************************************** */
                                                                         
-// $Revision: 1.20 $
-// $Date: 2001-11-26 22:53:54 $
+// $Revision: 1.21 $
+// $Date: 2002-01-06 19:41:50 $
 // $Source: /usr/local/cvs/OpenSees/SRC/element/nonlinearBeamColumn/element/NLBeamColumn2d.cpp,v $
                                                                         
                                                                         
@@ -1289,8 +1289,8 @@ NLBeamColumn2d::Print(ostream &s, int flag)
       double V = (Secommit(1)+Secommit(2))/L;
       theVector(1) = V;
       theVector(4) = -V;
-      s << "\tEnd 1 Forces (P V M): " << theVector(0) << ' ' << theVector(1) << ' ' << theVector(2) << endl;
-      s << "\tEnd 2 Forces (P V M): " << theVector(3) << ' ' << theVector(4) << ' ' << theVector(5) << endl;
+      s << "\tEnd 1 Forces (P V M): " << theVector(0) << " " << theVector(1) << " " << theVector(2) << endl;
+      s << "\tEnd 2 Forces (P V M): " << theVector(3) << " " << theVector(4) << " " << theVector(5) << endl;
    }
 }
 
