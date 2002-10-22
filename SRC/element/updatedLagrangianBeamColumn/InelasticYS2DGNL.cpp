b5171473743712e56ba119d3766fa02eac3d7de1
jeremic <jeremic@0eff8f49-d3cf-49fd-9ff1-3c36910be21e>
2002-10-22 22:44:39 +0000

    I took out the OpenGL rendering part as it was not made general so that the user can choose either X11 or OpenGL (through DEFs in Makefile.def). Rohit or Frank, please take a look and try to make this conditional compilation depending of what comes from Makefile.def. It compiles fine now with either OpenGL or X11 ... Boris Jeremic  (@ucdavis.edu)
    git-svn-id: svn://peera.berkeley.edu/usr/local/svn/OpenSees/trunk@1127 0eff8f49-d3cf-49fd-9ff1-3c36910be21e

--- SRC/element/updatedLagrangianBeamColumn/InelasticYS2DGNL.cpp
+++ SRC/element/updatedLagrangianBeamColumn/InelasticYS2DGNL.cpp
@@ -7,7 +7,7 @@
 //#include <Renderer.h>
 #include <math.h>
 #include <stdlib.h>
-#include <OpenGLRenderer.h>
+//tempout#include <OpenGLRenderer.h>
 #include "InelasticYS2DGNL.h"
 #include <Renderer.h>
 //#include <WindowManager.h>
@@ -1598,33 +1598,33 @@ void InelasticYS2DGNL::createView(char *title, double scale, int x, int y, int c
 {
 	displayType = displaytype;
 
-	theMap = new PlainMap();
-	pView =  new OpenGLRenderer(title, x, y, cx, cy, *theMap);
-	
-	if(pView)
-	{
-	pView->setVRP(0.0, 0.0, 0.0);
-	pView->setVPN(0.0, 0.0, 1.0);
-	pView->setVUP(0.0, 1.0, 0.0);
-	pView->setFillMode("wire");             // wire mode
-	pView->setPlaneDist(1.0, -1.0);
-	pView->setPRP(0.0, 0.0, 10.0);
-	pView->setPortWindow(-1, 1, -1, 1);  // use the whole window
-
-	pView->setViewWindow(-scale, scale, -scale, scale);
-
-	pView->clearImage();
-	pView->startImage();
-	
-
-	ys1->setView(pView);
-	ys2->setView(pView);
-	
-	ys1->displaySelf(*pView, 10, 1);
-	ys2->displaySelf(*pView, 10, 1);
-	pView->doneImage();
-	}
-	else
+//tempout	theMap = new PlainMap();
+//tempout	pView =  new OpenGLRenderer(title, x, y, cx, cy, *theMap);
+//tempout	
+//tempout	if(pView)
+//tempout	{
+//tempout	pView->setVRP(0.0, 0.0, 0.0);
+//tempout	pView->setVPN(0.0, 0.0, 1.0);
+//tempout	pView->setVUP(0.0, 1.0, 0.0);
+//tempout	pView->setFillMode("wire");             // wire mode
+//tempout	pView->setPlaneDist(1.0, -1.0);
+//tempout	pView->setPRP(0.0, 0.0, 10.0);
+//tempout	pView->setPortWindow(-1, 1, -1, 1);  // use the whole window
+//tempout
+//tempout	pView->setViewWindow(-scale, scale, -scale, scale);
+//tempout
+//tempout	pView->clearImage();
+//tempout	pView->startImage();
+//tempout	
+//tempout
+//tempout	ys1->setView(pView);
+//tempout	ys2->setView(pView);
+//tempout	
+//tempout	ys1->displaySelf(*pView, 10, 1);
+//tempout	ys2->displaySelf(*pView, 10, 1);
+//tempout	pView->doneImage();
+//tempout	}
+//tempout	else
 		cerr << "WARNING: InelasticYS2DGNL::createView - Renderer not available\n";
 }
 
