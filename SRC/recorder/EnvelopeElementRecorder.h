7b918ae4c3ca5bedb48b91b71e4c8fe37698ebd6
fmk <fmk@0eff8f49-d3cf-49fd-9ff1-3c36910be21e>
2002-12-13 00:06:18 +0000

    fmk - initial import of envelope recorders
    git-svn-id: svn://peera.berkeley.edu/usr/local/svn/OpenSees/trunk@1213 0eff8f49-d3cf-49fd-9ff1-3c36910be21e

---
+++ SRC/recorder/EnvelopeElementRecorder.h
@@ -0,0 +1,95 @@
+/* ****************************************************************** **
+**    OpenSees - Open System for Earthquake Engineering Simulation    **
+**          Pacific Earthquake Engineering Research Center            **
+**                                                                    **
+**                                                                    **
+** (C) Copyright 1999, The Regents of the University of California    **
+** All Rights Reserved.                                               **
+**                                                                    **
+** Commercial use of this program without express permission of the   **
+** University of California, Berkeley, is strictly prohibited.  See   **
+** file 'COPYRIGHT'  in main directory for information on usage and   **
+** redistribution,  and for a DISCLAIMER OF ALL WARRANTIES.           **
+**                                                                    **
+** Developed by:                                                      **
+**   Frank McKenna (fmckenna@ce.berkeley.edu)                         **
+**   Gregory L. Fenves (fenves@ce.berkeley.edu)                       **
+**   Filip C. Filippou (filippou@ce.berkeley.edu)                     **
+**                                                                    **
+** ****************************************************************** */
+                                                                        
+// $Revision: 1.1 $
+// $Date: 2002-12-13 00:06:14 $
+// $Source: /usr/local/cvs/OpenSees/SRC/recorder/EnvelopeElementRecorder.h,v $
+                                                                        
+#ifndef EnvelopeElementRecorder_h
+#define EnvelopeElementRecorder_h
+
+// Written: fmk 
+//
+// What: "@(#) EnvelopeElementRecorder.h, revA"
+
+#include <Recorder.h>
+#include <fstream.h>
+
+#include <Information.h>
+#include <G3Globals.h>
+#include <ID.h>
+
+
+class Domain;
+class Vector;
+class Matrix;
+class Element;
+class Response;
+class FE_Datastore;
+
+class EnvelopeElementRecorder: public Recorder
+{
+  public:
+    EnvelopeElementRecorder(const ID &eleID, 
+			    Domain &theDomain, 
+			    char **argv, 
+			    int argc,
+			    double deltaT = 0.0, 
+			    char *fileName =0);
+
+    EnvelopeElementRecorder(const ID &eleID, 
+			    Domain &theDomain, 
+			    char **argv, 
+			    int argc,
+			    FE_Datastore *db, 
+			    char *tableName, 
+			    double deltaT = 0.0);
+
+    ~EnvelopeElementRecorder();
+    int record(int commitTag, double timeStamp);
+    int playback(int commitTag);
+
+    void restart(void);    
+    
+  protected:
+    
+  private:	
+    int numEle;
+    ID responseID;                 // integer element returns in setResponse
+    Response **theResponses;
+
+    Domain *theDomain;
+    char *fileName;                // file name  
+    ofstream theFile; 	           // output stream
+
+    double deltaT;
+    double nextTimeStampToRecord;
+
+    FE_Datastore *db;
+    char **dbColumns;
+    int numDbColumns;
+
+    Matrix *data;
+    Vector *currentData;
+    bool first;
+};
+
+
+#endif
