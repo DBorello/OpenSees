fd3a38b2927e8a5a001725bfe15b74d57a9bff54
doolin <doolin@0eff8f49-d3cf-49fd-9ff1-3c36910be21e>
2004-02-03 00:20:14 +0000

    test
    git-svn-id: svn://peera.berkeley.edu/usr/local/svn/OpenSees/trunk@1683 0eff8f49-d3cf-49fd-9ff1-3c36910be21e

--- SRC/test.h
+++ SRC/test.h
@@ -1,5 +1,5 @@
 
 modiified
 
-and again modified again and again and one last time and again and again and again
+and again again and again and one last time and again and again and again
 just a test for access controls.
