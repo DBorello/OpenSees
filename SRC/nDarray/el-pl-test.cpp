685389913ed72de539a7b433fc2edd5432541dec
jeremic <jeremic@0eff8f49-d3cf-49fd-9ff1-3c36910be21e>
2004-05-15 19:07:18 +0000

    adding some more clues to the constitutive driver example. Boris Jeremic (@ucdavis.edu)
    git-svn-id: svn://peera.berkeley.edu/usr/local/svn/OpenSees/trunk@1777 0eff8f49-d3cf-49fd-9ff1-3c36910be21e

--- SRC/nDarray/el-pl-test.cpp
+++ SRC/nDarray/el-pl-test.cpp
@@ -171,48 +171,10 @@ else
   double Ey = 20000;
   double nu = 0.2;
 // building stiffness tensor in one command line
-  tensor E1 = (I_ijkl*((Ey*nu*2.0)/(2.0*(1.0+nu)*(1-2.0*nu)))) +
+  tensor Eel = (I_ijkl*((Ey*nu*2.0)/(2.0*(1.0+nu)*(1-2.0*nu)))) +
               ( (I_ikjl+I_iljk)*(Ey/(2.0*(1.0+nu))));
 // building compliance tensor in one command line
-  tensor D1= (I_ijkl * (-nu/Ey)) + ( (I_ikjl+I_iljk) * ((1.0+nu)/(2.0*Ey)));
-
-// multiplication between them
-  tensor test = E1("ijkl")*D1("klpq");
-  test.print("t","\n\n\n test =  E1(\"ijkl\")*D1(\"klpq\") \n");
-// and this should be equal to the symmetric part of 4. order unit tensor
-  if ( test == I4s ) ::printf("\n test == I4s  TRUE  ( up to sqrt(macheps())) \n");
-  else ::printf("\a\n\n\n   test == I4s    NOTTRUE  ( up to sqrt(macheps())) \n\n\n");
-
-//............Different Way...........................................
-// Linear Isotropic Elasticity Tensor
-  double lambda = nu * Ey / (1. + nu) / (1. - 2. * nu);
-  double mu = Ey / (2. * (1. + nu));
-  ::printf("\n  lambda = %.12e\n",lambda);
-  ::printf("      mu = %.12e\n",mu);
-
-  ::printf("\nYoung Modulus = %.4e",Ey);
-  ::printf("\nPoisson ratio = %.4e\n",nu);
-  ::printf(  "    lambda + 2 mu = %.4e\n", (lambda + 2 * mu));
-
-// building elasticity tensor
-  tensor E2 = (I_ijkl * lambda) + (I4s * (2. * mu));
-  E2.print("E2","tensor E2: elastic moduli Tensor = lambda*I2*I2+2*mu*I4s");
-
-  tensor E2inv = E2.inverse();
-
-// building compliance tensor
-  tensor D2= (I_ijkl * (-nu/Ey)) + (I4s * (1./(2.*mu)));
-  D2.print("D2","tensor D2: compliance Tensor (I_ijkl * (-nu/Ey)) + (I4s * (1./2./mu))");
-
-  if ( E2inv == D2 ) ::printf("\n E2inv == D2   TRUE \n");
-  else ::printf("\a\n\n\n  E2inv == D2   NOTTRUE \n\n\n");
-
-  if ( E1 == E2 ) ::printf("\n E1 == E2  TRUE \n");
-  else ::printf("\a\n\n\n  E1 == E2  NOTTRUE \n\n\n");
-
-  if ( D1 == D2 ) ::printf("\n D1 == D2  TRUE \n");
-  else ::printf("\a\n\n\n  D1 == D2  NOTTRUE \n\n\n");
-
+  tensor Del= (I_ijkl * (-nu/Ey)) + ( (I_ikjl+I_iljk) * ((1.0+nu)/(2.0*Ey)));
 
  
 double PIo3 = PI/3.0;
@@ -236,75 +198,100 @@ strain.report("strain = D2(\"ijkl\") * stress(\"kl\") \n");
 
 
 ::printf("\n\n\n\n\n");
-::printf("\n\n----------------- STRESS TENSOR --------(Kaspar Willam tests)--------\n\n");
+::printf("\n\n----------------- Derivatives of Yield function --------\n\n");
   
+
+
 	 static double stressvalues[] = {  1.0, 2.0, 3.0,
                                     2.0, 2.0, 5.0,
                                     3.0, 5.0, 9.0  };
 
-  stresstensor sigma(stressvalues);
-  sigma.report("\n\n stress tensor sigma (2nd-order with values assignment)");
+  stresstensor start_sigma(stressvalues);
+  start_sigma.report("\n\n starting stress tensor start_sigma )");
 
 
-	 stresstensor s = sigma.deviator();
-	 s.report("\n\n stress deviator s \n\n"); 
 
-	 stresstensor test01 = s("ij") * s("ij");
-	 test01.reportshort("\n\n\n test01 \n\n\n ");
-
-	 stresstensor KWtest01 =  s("ij") * s("jk");
-	 stresstensor KWtest02 =  KWtest01("ik") * I2("ik");
-	 KWtest02.reportshort("\n\n\n KWtest02 \n\n\n "); 
+	 static double strainvalues[] = {  0.1, 0.0, 0.0,
+                                    0.0, 0.1, 0.0,
+                                    0.0, 0.0, 0.1  };
+  straintensor epsilon(strainvalues);
+  epsilon.report("\n\n strain tensor epsilon (2nd-order with values assignment)");
 
 
-//  static double stressvector[] = {-3.0, -2.0, 5.0, 2.0, 3.0, 5.0};
-  static double stressvector[] = {-3.0, -2.0, 5.0, 2.0*sqrt(2.0), 3.0*sqrt(2.0), 5.0*sqrt(2.0)};
+  stresstensor stress_increment = E("ijpq") * strain_incr("pq");
+  stress_increment.null_indices();
+  stress_increment.report("\n\n stress increment"); 
+  
+  double f_start = 0.0;
+  double f_pred  = 0.0;
 
-// vector constructor
-  BJvector stressvector01( 6, stressvector);
-  stressvector01.print("sigma","\n\n stressvector01\n");
+  stresstensor elastic_predictor_stress = start_stress + stress_increment;
+  elastic_predictor_stress.report("\n\n elastic predictor stress"); 
+  
 
-	 BJvector product = stressvector01.transpose() * stressvector01;
-  product.print("product","\n\n product = stressvector01*stressvector01 \n");
+// Yield surface evaluation for von Mises
+//    double k = 10.0: //cohesion
+//    double k2 = k * k;
+//    stresstensor s = elastic_predictor_stress.deviator();     
+//    stresstensor temp1 = s("ij") * s("ij");
+//    double temp = temp1.trace();
+//    temp = temp * 3.0 / 2.0;
+//
+//    double f   = temp - k2;
 
+
+//    f_start = 
+    //::printf("\n##############  f_start = %.10e  ",f_start);
+
+//    f_pred =  
+    //::printf("##############  f_pred = %.10e\n\n",f_pred);
+
+
+    stresstensor dFods( 2, def_dim_2, 0.0);
+    //  stresstensor s;  // deviator
+    tensor temp1( 2, def_dim_2, 0.0);
+    tensor temp2( 2, def_dim_2, 0.0);
+    double lower = 0.0;
+    tensor temp3( 2, def_dim_2, 0.0);
+
+    double Delta_lambda = 0.0;
+
+
+    stresstensor plastic_stress;
+    straintensor plastic_strain;
+    stresstensor elastic_plastic_stress;
+    // ::printf("\n...... felpred = %lf\n",felpred);
+
+    if ( f_pred >= 0 ) 
+      {
+
+//        dFods = 
+
+//  tensor upperE1 = Eel("pqkl")*dQods("kl");
+//        upperE1.null_indices();
+//  tensor upperE2 = dFods("ij")*Eel("ijmn");
+//        upperE2.null_indices();
+//
+//  tensor upperE = upperE1("pq") * upperE2("mn");
+//        upperE.null_indices();
+//
+//
+//      temp2 = upperE2("ij")*dQods("ij"); // L_ij * E_ijkl * R_kl
+//        temp2.null_indices();
+//        lower = temp2.trace();
+//
+//        tensor Epl = upperE*(1./lower);
+//
+//        tensor Eep =  Eel - Epl;
+
+
+    }
 
 ::printf("\n\n\n");
 
 
 
 // --------------
-::printf("\n\n\n --------------------------TENSOR SCALAR MULTIPLICATION ----\n\n");
-  tensor EEEE1 = E2;
-  tensor EEEE2 = EEEE1*2.0;
-  EEEE1.print("E1","tensor EEEE1");
-  EEEE2.print("E2","tensor EEEE2");
-
-
-//###printf("\n\n------------VECTOR AND SKYMATRIX -----------------\n\n");
-//###// 1) use of member function full_val is of crucial importance here
-//###// and it enable us to use skymatrix as if it is full matrix.
-//###// There is however small time overhead but that can be optimized . . .
-//###double vect4[] = {1.0, 2.0, 3.0, 4.0, 5.0};
-//###
-//###matrix res2;
-//###vector vector2(5, vect4);
-//###second.full_print();
-//###vector2.print();
-//###res2 = vector2.transpose() * second;
-//###res2.print();
-//###
-//###
-//###printf("\n\n------------ MATRIX AND SKYMATRIX -----------------\n\n");
-//###// 1) use of member function full_val is of crucial importance here
-//###// and it enable us to use skymatrix as if it is full matrix.
-//###// There is however small time overhead but that can be optimized . . .
-//###
-//###matrix big_result;
-//###big_result = first_matrix * second;
-//###big_result.print();
-
-
-
 
   exit(1);
 //  return 1;
