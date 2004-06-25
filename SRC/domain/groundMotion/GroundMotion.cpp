8f069994a977008def6e84c0df3552bf6980e6c6
fmk <fmk@0eff8f49-d3cf-49fd-9ff1-3c36910be21e>
2004-06-25 22:25:25 +0000

    fmk - adding a warining message if integration is required to get ground displacements
    git-svn-id: svn://peera.berkeley.edu/usr/local/svn/OpenSees/trunk@1819 0eff8f49-d3cf-49fd-9ff1-3c36910be21e

--- SRC/domain/groundMotion/GroundMotion.cpp
+++ SRC/domain/groundMotion/GroundMotion.cpp
@@ -18,8 +18,8 @@
 **                                                                    **
 ** ****************************************************************** */
                                                                         
-// $Revision: 1.6 $
-// $Date: 2003-03-04 00:48:10 $
+// $Revision: 1.7 $
+// $Date: 2004-06-25 22:25:25 $
 // $Source: /usr/local/cvs/OpenSees/SRC/domain/groundMotion/GroundMotion.cpp,v $
                                                                         
 // Written: fmk 
@@ -205,6 +205,7 @@ GroundMotion::getVel(double time)
   
   // if theAccel is not 0, integrate accel series to get a vel series
   else if (theAccelSeries != 0) {
+    opserr << " WARINNG: GroundMotion::getVel(double time) - we are having to integrate to get the ground velocities, are you happy with the integration scheme?\n";
     theVelSeries = this->integrate(theAccelSeries, delta);
     if (theVelSeries != 0)
       return theVelSeries->getFactor(time);      
@@ -226,6 +227,7 @@ GroundMotion::getDisp(double time)
 
   // if theVel is not 0, integrate vel series to get disp series
   else if (theVelSeries != 0) {
+    opserr << " WARINNG: GroundMotion::getDisp(double time) - we are having to integrate the velocities get the ground displacements, are you happy with the integration scheme?\n";
     theDispSeries = this->integrate(theVelSeries, delta);
     if (theDispSeries != 0)
       return theDispSeries->getFactor(time);      
@@ -234,7 +236,8 @@ GroundMotion::getDisp(double time)
   }
 
   // if theAccel is not 0, integrate vel series to get disp series
-  else if (theAccelSeries != 0) {
+  else if (theAccelSeries != 0) {    
+    opserr << " WARINNG: GroundMotion::getDisp(double time) - we are having to integrate the acceleration time series to get the velocities and then integrate these to get to get the ground displacements, are you happy with the integration scheme?\n";
     theVelSeries = this->integrate(theAccelSeries, delta);
     if (theVelSeries != 0) {
       theDispSeries = this->integrate(theVelSeries, delta);
