ba7b286b2e01bba3f232cfee8f8e131ade2a93fd
jeremic <jeremic@0eff8f49-d3cf-49fd-9ff1-3c36910be21e>
2005-05-17 21:06:19 +0000

    small changes, mostly formating, Boris Jeremic (@ucdavis.edu)
    git-svn-id: svn://peera.berkeley.edu/usr/local/svn/OpenSees/trunk@2032 0eff8f49-d3cf-49fd-9ff1-3c36910be21e

--- SRC/material/nD/Template3Dep/RMC01_PS.h
+++ SRC/material/nD/Template3Dep/RMC01_PS.h
@@ -1,38 +1,38 @@
-//================================================================================
-//# COPY LEFT and RIGHT:                                                         #
-//# Commercial    use    of    this  program without express permission of the   #
-//# University  of  California, is strictly encouraged. Copyright and Copyleft   #
-//# are covered by the following clause:                                         #
-//#                                                                              #
-//# Woody's license:                                                             #
-//# ``This    source    code is Copyrighted in U.S., by the The Regents of the   #
-//# University  of  California,  for  an indefinite period, and anybody caught   #
-//# using  it  without  our  permission,  will be mighty good friends of ourn,   #
-//# cause  we  don't give a darn. Hack it. Compile it. Debug it. Run it. Yodel   #
-//# it. Enjoy it. We wrote it, that's all we wanted to do.'' bj                  #
-//#                                                                              #
-//#                                                                              #
-//#                                                                              #
-//# PROJECT:           Object Oriented Finite Element Program                    #
-//# PURPOSE:           Rounded Mohr Coulomb Potential Surface                    #
-//# CLASS:                                                                       #
-//#                                                                              #
-//# VERSION:                                                                     #
-//# LANGUAGE:          C++                                                       #
-//# TARGET OS:         DOS || UNIX || . . .                                      #
-//# DESIGNER(S):       Boris Jeremic jeremic@ucdavis.edu                         #
-//#                    Zhao Cheng,                                               #
-//# PROGRAMMER(S):     Zhao Cheng, Boris Jeremic                                 #
-//#                                                                              #
-//#                                                                              #
-//# DATE:              12 Feb. 2003                                              #
-//# UPDATE HISTORY:                                                              #
-//#                                                                              #
-//#                                                                              #
-//#                                                                              #
-//#                                                                              #
-//# SHORT EXPLANATION: Functions for rounded Mohr-Coulomb potential function     #
-//#                                                                              #
+//
+// COPY LEFT and RIGHT:
+// Commercial    use    of    this  program without express permission of the
+// University, is strictly encouraged. Copyright and Copyleft are covered
+// by the following clause:
+//
+// Woody's (Guthrie) license:																																		
+// ``This    source    code is Copyrighted in U.S., by the The University and 
+// by the Authors,  for  an indefinite period, and anybody caught
+// using  it  without  our  permission,  will be mighty good friends of ourn,
+// cause  we  don't give a darn. Hack it. Compile it. Debug it. Run it. Yodel
+// it. Enjoy it. We wrote it, that's all we wanted to do.'' bj
+//
+//
+//
+// PROJECT:           Object Oriented Finite Element Program
+// PURPOSE:           Rounded Mohr Coulomb Potential Surface
+// CLASS:
+//
+// VERSION:
+// LANGUAGE:          C++
+// TARGET OS:         DOS || UNIX || . . .
+// DESIGNER(S):       Boris Jeremic jeremic@ucdavis.edu
+//                    Zhao Cheng,
+// PROGRAMMER(S):     Zhao Cheng, Boris Jeremic
+//
+//
+// DATE:              12 Feb. 2003
+// UPDATE HISTORY:
+//
+//
+//
+//
+// SHORT EXPLANATION: Functions for rounded Mohr-Coulomb potential function
+//
 //================================================================================
 
 #ifndef RMC01_PS_H
@@ -58,14 +58,14 @@ class RMC01PotentialSurface : public PotentialSurface
 
     tensor dQods(const EPState *EPS  ) const;
     tensor d2Qods2(const EPState *EPS) const;
-    
-    tensor d2Qodsds1(const EPState *EPS) const; // For Consistent Algorithm, Z Cheng, Jan 2004    
-    
+
+    tensor d2Qodsds1(const EPState *EPS) const; // For Consistent Algorithm, Z Cheng, Jan 2004
+
     void print() { opserr << *this; };
 
     //================================================================================
     // Overloaded Insertion Operator
-    // prints an RMC01-PotentialSurface's contents 
+    // prints an RMC01-PotentialSurface's contents
     //================================================================================
     friend OPS_Stream& operator<< (OPS_Stream& os, const RMC01PotentialSurface &PS);
 
