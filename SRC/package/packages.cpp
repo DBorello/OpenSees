13e7c3898bc21af8d9c74e95136ee8b5e5013c03
fmk <fmk@0eff8f49-d3cf-49fd-9ff1-3c36910be21e>
2005-06-14 19:05:11 +0000

    fmk - adding function to be called for loading of dynmaic library and function
    git-svn-id: svn://peera.berkeley.edu/usr/local/svn/OpenSees/trunk@2047 0eff8f49-d3cf-49fd-9ff1-3c36910be21e

---
+++ SRC/package/packages.cpp
@@ -0,0 +1,49 @@
+
+#include <stdlib.h>
+#include <string.h>
+#include <OPS_Globals.h>
+
+#ifdef _WIN32
+
+#else
+
+#include <dlfcn.h>
+#endif
+
+int 
+getLibraryFunction(const char *libName, const char *funcName, void **libHandle, void **funcHandle) {
+
+  int result = 0;
+
+  *libHandle = NULL;
+  *funcHandle = NULL;
+
+#ifdef _WIN32
+  
+#else
+  
+  int libNameLength = strlen(libName);
+  char *localLibName = new char[libNameLength+4];
+  strcpy(localLibName, libName);
+  strcpy(&localLibName[libNameLength], ".so");
+
+  char *error;
+  *libHandle = dlopen (localLibName, RTLD_NOW);
+  if (*libHandle != NULL) {    
+
+    *funcHandle = dlsym(*libHandle, funcName);
+    if ((error = dlerror()) != NULL) {
+      opserr << *error;
+      result = -2;
+    } else
+  } else
+    result = -1;
+
+  delete [] localLibName;
+  
+#endif
+
+  return result;
+}
+
+
