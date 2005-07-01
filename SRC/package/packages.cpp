75d61069f846b7cf153bee1a9d5aee3968d233f7
fmk <fmk@0eff8f49-d3cf-49fd-9ff1-3c36910be21e>
2005-07-01 20:00:07 +0000

    fmk - changes to load windows dll
    git-svn-id: svn://peera.berkeley.edu/usr/local/svn/OpenSees/trunk@2060 0eff8f49-d3cf-49fd-9ff1-3c36910be21e

--- SRC/package/packages.cpp
+++ SRC/package/packages.cpp
@@ -3,10 +3,11 @@
 #include <string.h>
 #include <OPS_Globals.h>
 
+
 #ifdef _WIN32
 
+#include <windows.h>
 #else
-
 #include <dlfcn.h>
 #endif
 
@@ -19,7 +20,59 @@ getLibraryFunction(const char *libName, const char *funcName, void **libHandle, 
   *funcHandle = NULL;
   
 #ifdef _WIN32
-  
+
+	//
+	// first try and open dll
+	//
+
+	int libNameLength = strlen(libName);
+	char *localLibName = new char[libNameLength+5];
+	strcpy(localLibName, libName);
+	strcpy(&localLibName[libNameLength], ".dll");
+	
+	HINSTANCE hLib = LoadLibrary(localLibName);
+	
+	delete [] localLibName;
+
+	if (hLib != NULL) {
+opserr << "FOUND LIB\n";
+		char mod[124];
+		GetModuleFileName((HMODULE)hLib, (LPTSTR)mod, 124);
+	
+		//
+		// now look for function with funcName
+		//
+
+		(*funcHandle) = (void *)GetProcAddress((HMODULE)hLib, funcName);
+	
+		if (*funcHandle == NULL) {
+			FreeLibrary((HMODULE)hLib);
+			return -2;
+		}	
+
+		opserr << "FOUND FUNC\n";
+
+		//
+		// we need to set the OpenSees pointer global variables if function there
+		//
+
+	    typedef void(_cdecl *setGlobalPointersFunction)(OPS_Stream *);
+	    setGlobalPointersFunction funcPtr;
+	
+		// look for pointer function
+		funcPtr = (setGlobalPointersFunction)GetProcAddress((HMODULE)hLib,"setGlobalPointers");
+		if (!funcPtr) {
+			FreeLibrary((HMODULE)hLib);
+			return -2;
+		}
+
+		// invoke pointer function
+		(funcPtr)(opserrPtr);
+
+	} else // no lib exists
+		return -1;
+
+	libHandle =  (void **)&hLib;
 #else
   
   int libNameLength = strlen(libName);
@@ -42,6 +95,6 @@ getLibraryFunction(const char *libName, const char *funcName, void **libHandle, 
   delete [] localLibName;
   
 #endif
-  
+ 
   return result;
 }
