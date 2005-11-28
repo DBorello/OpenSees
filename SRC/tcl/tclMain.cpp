b48f733c77fda956ac225987132fe8248147b293
fmk <fmk@0eff8f49-d3cf-49fd-9ff1-3c36910be21e>
2005-11-28 22:23:40 +0000

    fmk - changing version number to 1.6.2.f
    git-svn-id: svn://peera.berkeley.edu/usr/local/svn/OpenSees/trunk@2190 0eff8f49-d3cf-49fd-9ff1-3c36910be21e

--- SRC/tcl/tclMain.cpp
+++ SRC/tcl/tclMain.cpp
@@ -9,7 +9,7 @@
  * See the file "license.terms" for information on usage and redistribution
  * of this file, and for a DISCLAIMER OF ALL WARRANTIES.
  *
- * RCS: @(#) $Id: tclMain.cpp,v 1.24 2005-10-24 19:45:08 fmk Exp $
+ * RCS: @(#) $Id: tclMain.cpp,v 1.25 2005-11-28 22:23:40 fmk Exp $
  */
 
 /*                       MODIFIED   FOR                              */
@@ -144,7 +144,7 @@ g3TclMain(int argc, char **argv, Tcl_AppInitProc * appInitProc)
 
     /* fmk - beginning of modifications for OpenSees */
     fprintf(stderr,"\n\n\t OpenSees -- Open System For Earthquake Engineering Simulation");
-    fprintf(stderr,"\n\tPacific Earthquake Engineering Research Center -- Version 1.6.2.e\n\n");
+    fprintf(stderr,"\n\tPacific Earthquake Engineering Research Center -- Version 1.6.2.f\n\n");
     
     fprintf(stderr,"\t    (c) Copyright 1999 The Regents of the University of California");
     fprintf(stderr,"\n\t\t\t\t All Rights Reserved \n\n\n");    
