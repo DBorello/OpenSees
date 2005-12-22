04eb6c605e618f194ccab007fb8408d223548cd7
fmk <fmk@0eff8f49-d3cf-49fd-9ff1-3c36910be21e>
2005-12-22 01:04:35 +0000

    fmk - changes
    git-svn-id: svn://peera.berkeley.edu/usr/local/svn/OpenSees/trunk@2241 0eff8f49-d3cf-49fd-9ff1-3c36910be21e

--- SRC/database/main.cpp
+++ SRC/database/main.cpp
@@ -18,13 +18,11 @@
 **                                                                    **
 ** ****************************************************************** */
                                                                         
-// $Revision: 1.3 $
-// $Date: 2003-02-25 23:32:40 $
+// $Revision: 1.4 $
+// $Date: 2005-12-22 01:04:35 $
 // $Source: /usr/local/cvs/OpenSees/SRC/database/main.cpp,v $
                                                                         
                                                                         
-// File: ~/model/main.C
-//
 // Written: fmk 12/95
 // Revised:
 //
@@ -38,7 +36,6 @@
 #include <OPS_Globals.h>
 #include <Timer.h>
 #include <FileDatastore.h>
-#include <Quick2dFrame.h>
 #include <Node.h>
 #include <NodeIter.h>
 #include <Element.h>
@@ -49,69 +46,196 @@
 #include <Vector.h>
 #include <Matrix.h>
 #include <FEM_ObjectBroker.h>
+#include <bool.h>
 
+double ops_Dt;
+Domain * ops_TheActiveDomain;
 #include <StandardStream.h>
 StandardStream sserr;
-OPS_Stream &opserr = sserr;
+OPS_Stream *opserrPtr = &sserr;
 
 int main(int argc, char **argv)
 {
+  bool fail = false;
 
-    //
-    //	now create a domain and a modelbuilder
-    //  and build the model
-    //
-    FEM_ObjectBroker theBroker;
-    Domain *theDomain = new Domain();
-    ModelBuilder  *theModelBuilder = new Quick2dFrame(*theDomain);
+  //
+  //	now create a domain and a modelbuilder
+  //  and build the model
+  //
+  FEM_ObjectBroker theBroker;
+  Domain *theDomain = new Domain();
+  
+  FileDatastore *theDatabase 
+    = new FileDatastore("/tmp/database/test1",*theDomain,theBroker);
+  FileDatastore &theDb = *theDatabase;
+  
+  opserr << "TESTING IDs: \n";
+  
+  ID id1(2);
+  id1(0) = 1; id1(1) = 1;
+  ID id0(2);
+  id0(0) = 0; id0(1) = 0;
+  ID id2(2);
+  id2(0) = 2; id2(1) = 2;
+  ID id3(2);
+  id3(0) = 3; id3(1) = 3;
+  ID id4(2);
+  id4(0) = 4; id4(1) = 4;
+  ID id5(2);
+  id5(0) = 5; id5(1) = 5;
+  ID id6(2);
+  id6(0) = 6; id6(1) = 6;
+  
+  
+  theDb.sendID(1,1,id1);
+  theDb.sendID(1,2,id2);
+  theDb.sendID(2,1,id3);
+  theDb.sendID(2,2,id4);
+  
+  opserr << "RESULTS\n";    
+  ID recvID(2);
+  theDb.recvID(1,1,recvID);
+  opserr << "1: " << recvID;
+  theDb.recvID(1,2,recvID);
+  opserr << "2: " << recvID;
+  theDb.recvID(2,1,recvID);
+  opserr << "3: " << recvID;
+  theDb.recvID(2,2,recvID);
+  opserr << "4: " << recvID;
+  
+  theDb.sendID(1,1,id1);
+  theDb.sendID(3,1,id3);
+  theDb.sendID(2,1,id2);
+  theDb.sendID(0,1,id0);
+  theDb.sendID(1,2,id1);
+  theDb.sendID(2,2,id2);
+  theDb.sendID(3,1,id3);
+  theDb.sendID(5,1,id5);
+  theDb.sendID(4,1,id4);
+  theDb.sendID(1,1,id1);
+  
+  
+  theDb.recvID(3,1,id5);
+  opserr << "3: " << id5;
+  theDb.recvID(1,1,id5);
+  opserr << "1: " << id5;
+  theDb.recvID(2,1,id5);
+  opserr << "2: " << id5;
+  theDb.recvID(1,2,id5);
+  opserr << "1: " << id5;
+  theDb.recvID(2,2,id5);
+  opserr << "3: " << id5;
+  theDb.recvID(3,1,id5);
+  opserr << "3: " << id5;
+  theDb.recvID(4,1,id5);
+  opserr << "4: " << id5;
+  theDb.recvID(5,1,id5);
+  opserr << "5: " << id5;
+  theDb.recvID(6,1,id5);
+  opserr << "FAILURE\n";
+  theDb.recvID(6,1,id5);
+  opserr << "FAILURE\n";
+  theDb.recvID(0,1,id5);
+  opserr << "0: " << id5;
+  theDb.recvID(5,1,id5);
+  opserr << "5: " << id5;
+  
+  
+  ID id64(4);
+  id64(0) = 6; id64(1) = 6; id64(2) = 6; id64(3) = 6;
+  theDb.sendID(6,1,id64);
+  theDb.recvID(6,1,id64);
+  opserr << id64;
+  
 
-    FileDatastore *theDatabase 
-      = new FileDatastore("/usr/users/fmk/g3/SRC/database/test1",*theDomain,theBroker);
-    FileDatastore &theDb = *theDatabase;
+  opserr << "TESTING MATRICES: \n";
 
-  /*
-    opserr << "TESTING IDs: \n";
-    ID id1(2);
-    id1(0) = 1; id1(1) = 2;
+  Matrix mat1(2,2);
+  mat1(0,0) = 1.1; mat1(0,1) = 11.1;
+  mat1(1,0) = 111.1; mat1(1,1) = 1111.1;
+  
+  Matrix mat2(2,2);
+  mat2(0,0) = 2.2; mat2(0,1) = 22.2;
+  mat2(1,0) = 222.2; mat2(1,1) = 2222.2;
+  
+  theDb.sendMatrix(2,1,mat2);
+  theDb.sendMatrix(1,1,mat1);
+  theDb.sendMatrix(3,2,mat2);
+  theDb.sendMatrix(3,1,mat1);
+  
+  Matrix mat3(2,2);
+  theDb.recvMatrix(1,1,mat3);
+  opserr << mat1 << mat3 << endln;
+  theDb.recvMatrix(2,1,mat3);
+  opserr << mat2 << mat3 << endln;
+  theDb.recvMatrix(3,2,mat3);
+  opserr << mat2 << mat3 << endln;
+  theDb.recvMatrix(3,1,mat3);
+  opserr << mat1 << mat3 << endln;
+  
+  //    theDb.sendMatrix(2,1,mat1);
+  theDb.recvMatrix(2,1,mat3);
+  opserr << mat2 << mat3;
+  
+  
+    
+    opserr << "TESTING VECTORS: \n";
+    
+    Vector vect1(2);
+    vect1(0) = 1.1; vect1(1) = 2.22;
+    
+    Vector vect2(2);
+    vect2(0) = 3; vect2(1) = 4.2;
+    
+    Vector vect3(2);
+    vect3(0) = 5; vect3(1) = 6;
 
+    Vector vect4(2);
+    vect4(0) = 7; vect4(1) = 8.8e12;
 
-    ID id2(2);
-    id2(0) = 3; id2(1) = 4;
-
-    ID id3(2);
-    id3(0) = 5; id3(1) = 6;
-
-    ID id4(2);
-    id4(0) = 7; id4(1) = 8;
-
-    theDb.sendID(1,1,id1);
-    theDb.sendID(1,2,id2);
-    theDb.sendID(2,1,id3);
-    theDb.sendID(2,2,id4);
-    theDb.sendID(1,1,id4);
-    theDb.sendID(1,2,id3);
-    theDb.sendID(2,1,id2);
-    theDb.sendID(2,2,id1);
+    theDb.sendVector(1,1,vect1);
+    theDb.sendVector(1,2,vect2);
+    theDb.sendVector(2,1,vect3);
+    theDb.sendVector(2,2,vect4);
 
     opserr << "RESULTS\n";    
-    ID id5(2);
-    theDb.recvID(1,1,id5);
-    opserr << id5;
-    theDb.recvID(1,2,id5);
-    opserr << id5;
-    theDb.recvID(2,1,id5);
-    opserr << id5;
-    theDb.recvID(2,2,id5);
-    opserr << id5;
-    theDb.recvID(1,1,id5);
-    opserr << id5;
-    theDb.recvID(1,2,id5);
-    opserr << id5;
-    theDb.recvID(2,1,id5);
-    opserr << id5;
-    theDb.recvID(2,2,id5);
-    opserr << id5;
+    Vector vect5(2);
+    theDb.recvVector(1,1,vect5);
+    opserr << vect1 << vect5 << endln;
+    theDb.recvVector(1,2,vect5);
+    opserr << vect2 << vect5 << endln;
+    theDb.recvVector(2,1,vect5);
+    opserr << vect3 << vect5 << endln;
+    theDb.recvVector(2,2,vect5);
+    opserr << vect4 << vect5 << endln;
 
+    theDb.sendVector(2,2,vect1);
+    theDb.sendVector(2,1,vect2);
+    theDb.sendVector(1,2,vect3);
+    theDb.sendVector(1,1,vect4);
+
+    theDb.recvVector(1,1,vect5);
+    opserr << vect4 << vect5 << endln;
+    theDb.recvVector(1,2,vect5);
+    opserr << vect3 << vect5 << endln;
+    theDb.recvVector(2,1,vect5);
+    opserr << vect2 << vect5 << endln;
+    theDb.recvVector(2,2,vect5);
+    opserr << vect1 << vect5 << endln;
+
+
+    theDb.sendVector(4,4,vect5);
+    theDb.recvVector(4,4,vect5);
+    opserr << vect5 << vect5 << endln;
+
+    theDb.recvVector(5,5,vect5);
+    opserr << "FAIL\n";
+
+    theDatabase->commitState(0);
+    exit(0);
+
+
+    /*
     theDb.sendID(2,2,id1);
     theDb.sendID(2,1,id2);
     theDb.sendID(1,2,id3);
@@ -135,88 +259,15 @@ int main(int argc, char **argv)
     */
   
     /**************************
-    opserr << "TESTING VECTORS: \n";
 
-    Vector vect1(2);
-    vect1(0) = 1.11111; vect1(1) = 2.222e3;
-
-    Vector vect2(2);
-    vect2(0) = 3.33333; vect2(1) = 4.2;
-
-    Vector vect3(2);
-    vect3(0) = 5; vect3(1) = 6;
-
-    Vector vect4(2);
-    vect4(0) = 7; vect4(1) = 8.8e12;
-
-    theDb.sendVector(1,1,vect1);
-    theDb.sendVector(1,2,vect2);
-    theDb.sendVector(2,1,vect3);
-    theDb.sendVector(2,2,vect4);
-
-    opserr << "RESULTS\n";    
-    Vector vect5(2);
-    theDb.recvVector(1,1,vect5);
-    opserr << vect5;
-    theDb.recvVector(1,2,vect5);
-    opserr << vect5;
-    theDb.recvVector(2,1,vect5);
-    opserr << vect5;
-    theDb.recvVector(2,2,vect5);
-    opserr << vect5;
-
-    theDb.sendVector(2,2,vect1);
-    theDb.sendVector(2,1,vect2);
-    theDb.sendVector(1,2,vect3);
-    theDb.sendVector(1,1,vect4);
-
-    theDb.recvVector(1,1,vect5);
-    opserr << vect5;
-    theDb.recvVector(1,2,vect5);
-    opserr << vect5;
-    theDb.recvVector(2,1,vect5);
-    opserr << vect5;
-    theDb.recvVector(2,2,vect5);
-    opserr << vect5;
-
-
-    theDb.sendVector(4,4,vect5);
-    theDb.recvVector(4,4,vect5);
-    opserr << vect5;
-
-    theDb.recvVector(5,5,vect5);
-    opserr << vect5;
     **************************/
     /*
-    opserr << "TESTING MATRICES: \n";
 
-    Matrix mat1(2,2);
-    mat1(0,0) = 1.11111; mat1(0,1) = 2.222e3;
-    mat1(1,0) = 3.3; mat1(1,1) = 4.44;
-
-
-    Matrix mat2(mat1);
-    mat2 *= 10;
-    theDb.sendMatrix(2,1,mat2);
-
-    theDb.sendMatrix(1,1,mat1);
-
-    Matrix mat3(2,2);
-    theDb.recvMatrix(1,1,mat3);
-    opserr << mat3;
-    
-
-    theDb.recvMatrix(2,1,mat3);
-    opserr << mat3;
-
-    theDb.sendMatrix(2,1,mat1);
-    theDb.recvMatrix(2,1,mat3);
-    opserr << mat3;
     */
 
 //  theModelBuilder->buildFE_Model();
 //  theDb.commitState(0);
-  theDb.restoreState(0);
+//  theDb.restoreState(0);
 
 
     // theDb.restoreElements(0);
@@ -231,7 +282,7 @@ int main(int argc, char **argv)
     theDb.restoreElement(2,0);
     */
 
-  opserr << *theDomain;
+    //  opserr << *theDomain;
 
     delete theDatabase;
 
