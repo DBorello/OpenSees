99b49a16087c7e0d1a16d4116112e7794adef06a
fmk <fmk@0eff8f49-d3cf-49fd-9ff1-3c36910be21e>
2006-04-12 17:52:00 +0000

    m.berry - bug fixes
    git-svn-id: svn://peera.berkeley.edu/usr/local/svn/OpenSees/trunk@2394 0eff8f49-d3cf-49fd-9ff1-3c36910be21e

--- SRC/material/uniaxial/Concrete04.h
+++ SRC/material/uniaxial/Concrete04.h
@@ -18,10 +18,9 @@
 **                                                                    **
 ** ****************************************************************** */
                                                                         
-// $Revision: 1.1 $
-// $Date: 2005-09-23 22:51:27 $
+// $Revision: 1.2 $
+// $Date: 2006-04-12 17:52:00 $
 // $Source: /usr/local/cvs/OpenSees/SRC/material/uniaxial/Concrete04.h,v $
-                                                                        
                                                                         
 #ifndef Concrete04_h
 #define Concrete04_h
@@ -42,87 +41,89 @@
 
 #include <UniaxialMaterial.h>
 
+
 class Concrete04 : public UniaxialMaterial
 {
  public:
-//  Concrete04 (int tag, double fpc, double eco, double ecu, double Ec0, double fct);
-  Concrete04 (int tag, double fpc, double eco, double ecu, double Ec0, double fct, double etu);  
+// Concrete04 (int tag, double fpc, double eco, double ecu, double Ec0, double fct);
+  Concrete04 (int tag, double fpc, double eco, double ecu, double Ec0, double fct, double etu);
   Concrete04 (int tag, double fpc, double eco, double ecu, double Ec0, double fct, double etu, double beta);
   Concrete04 (int tag, double fpc, double eco, double ecu, double Ec0);
   Concrete04 ();
   ~Concrete04();
 
-      int setTrialStrain(double strain, double strainRate = 0.0); 
-      double getStrain(void);      
-      double getStress(void);
-      double getTangent(void);
-      double getInitialTangent(void) {return Ec0;}
-
-      int commitState(void);
-      int revertToLastCommit(void);
-      int revertToStart(void);        
-
-      UniaxialMaterial *getCopy(void);
-    
-      int sendSelf(int commitTag, Channel &theChannel);  
-      int recvSelf(int commitTag, Channel &theChannel, 
-	  	 FEM_ObjectBroker &theBroker);    
-    
-      void Print(OPS_Stream &s, int flag =0);
-
-	  	  	  // LOWES: add function for use with variable hinge lnegth model
-	  int getMaterialType(void);
-      // LOWES: end
-
-   protected:
-
-   private:
-      /*** Material Properties ***/
-      double fpc;    // Compressive strength
-      double epsc0;  // Strain at compressive strength
-      double epscu;  // Strain at crushing strength
-      double Ec0;    // initial tangent
-      double fct;   // Concrete tensile strength
-	  double etu;   // ultimate tensile strain              
-	  double beta;  // exponential curve parameter, residual stress (as a factor of ft)
-	                // at etu. 
-
-      /*** CONVERGED History Variables ***/
-      double CminStrain;   // Smallest previous concrete strain (compression)
-      double CunloadSlope; // Unloading (reloading) slope from CminStrain
-      double CendStrain;   // Strain at the end of unloading from CminStrain
-	  double CcompStrain;   // strain value at which the compression unloading intersects the   
-	                       // zero stress value or the strain value at which tensile reloading starts.                                  
-	  double CUtenStress;      // tensile stress value at which unloading begins
-	  double CUtenSlope;      // unloading tensile slope value
-
-      /*** CONVERGED State Variables ***/
-      double Cstrain;
-      double Cstress;   
-      double Ctangent;	// Don't need Ctangent other than for revert and sendSelf/recvSelf
-						// Storing it is better than recomputing it!!!
-
-      /*** TRIAL History Variables ***/
-      double TminStrain;
-      double TunloadSlope;
-      double TendStrain;
-	  double TcompStrain;
-	  double TUtenStress;
-	  double TUtenSlope;
-
-      /*** TRIAL State Variables ***/
-      double Tstrain;
-      double Tstress;
-      double Ttangent; // Not really a state variable, but declared here
-                       // for convenience
-
-      void CompReload(void);
-      void CompEnvelope(void);
-      void setCompUnloadEnv(void);
-      void TensReload(void);
-      void TensEnvelope(void);
-	  void setTenUnload(void);
+  int setTrialStrain(double strain, double strainRate = 0.0);
+  double getStrain(void);
+  double getStress(void);
+  double getTangent(void);
+  double getInitialTangent(void) {return Ec0;}
+  
+  int commitState(void);
+  int revertToLastCommit(void);
+  int revertToStart(void);
+  
+  UniaxialMaterial *getCopy(void);
+  
+  int sendSelf(int commitTag, Channel &theChannel);
+  int recvSelf(int commitTag, Channel &theChannel,
+	       FEM_ObjectBroker &theBroker);
+  
+  void Print(OPS_Stream &s, int flag =0);
+  
+ protected:
+  
+ private:
+  /*** Material Properties ***/
+  double fpc; // Compressive strength
+  double epsc0; // Strain at compressive strength
+  double epscu; // Strain at crushing strength
+  double Ec0; // initial tangent
+  double fct; // Concrete tensile strength
+  double etu; // ultimate tensile strain
+  double beta; // exponential curve parameter, residual stress (as a factor of ft)
+  // at etu.
+  
+  /*** CONVERGED History Variables ***/
+  double CminStrain; // Smallest previous concrete strain (compression)
+  double CmaxStrain;
+  double CunloadSlope; // Unloading (reloading) slope from CminStrain
+  double CendStrain; // Strain at the end of unloading from CminStrain
+  double CcompStrain; // strain value at which the compression unloading intersects the
+  // zero stress value or the strain value at which tensile reloading starts.
+  double CUtenStress; // tensile stress value at which unloading begins
+  double CUtenSlope; // unloading tensile slope value
+  
+  /*** CONVERGED State Variables ***/
+  double Cstrain;
+  double Cstress;
+  double Ctangent; // Don't need Ctangent other than for revert and sendSelf/recvSelf
+  // Storing it is better than recomputing it!!!
+  
+  double TminStrain;
+  /*** TRIAL History Variables ***/
+  double TmaxStrain;
+  double TunloadSlope;
+  double TendStrain;
+  double TcompStrain;
+  double TUtenStress;
+  double TUtenSlope;
+  
+  /*** TRIAL State Variables ***/
+  double Tstrain;
+  double Tstress;
+  double Ttangent; // Not really a state variable, but declared here
+  // for convenience
+  
+  void CompReload(void);
+  void CompEnvelope(void);
+  void setCompUnloadEnv(void);
+  void TensReload(void);
+  void TensEnvelope(void);
+  void setTenUnload(void);
 };
+
+
+
 
 
 #endif
