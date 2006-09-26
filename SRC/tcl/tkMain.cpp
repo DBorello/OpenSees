d4324a40ee023cb038df96dd2d28ad1253de5be3
fmk <fmk@0eff8f49-d3cf-49fd-9ff1-3c36910be21e>
2006-09-26 18:58:12 +0000

    fmk - replacing vesrion number with OPS_VERSION found in OPS_Globals.h
    git-svn-id: svn://peera.berkeley.edu/usr/local/svn/OpenSees/trunk@2595 0eff8f49-d3cf-49fd-9ff1-3c36910be21e

--- SRC/tcl/tkMain.cpp
+++ SRC/tcl/tkMain.cpp
@@ -14,7 +14,7 @@
  * See the file "license.terms" for information on usage and redistribution
  * of this file, and for a DISCLAIMER OF ALL WARRANTIES.
  *
- * RCS: @(#) $Id: tkMain.cpp,v 1.18 2006-08-11 23:36:00 fmk Exp $
+ * RCS: @(#) $Id: tkMain.cpp,v 1.19 2006-09-26 18:58:12 fmk Exp $
  */
 
 /*                       MODIFIED   FOR                              */
@@ -146,7 +146,7 @@ Tk_MainOpenSees(int argc, char **argv, Tcl_AppInitProc *appInitProc, Tcl_Interp 
 
     /* fmk - beginning of modifications for OpenSees */
     fprintf(stderr,"\n\n\t OpenSees -- Open System For Earthquake Engineering Simulation");
-    fprintf(stderr,"\n\tPacific Earthquake Engineering Research Center -- Version 1.7.1\n\n");
+    fprintf(stderr,"\n\tPacific Earthquake Engineering Research Center -- %s\n\n", OPS_VERSION);
     
     fprintf(stderr,"\t    (c) Copyright 1999 The Regents of the University of California");
     fprintf(stderr,"\n\t\t\t\t All Rights Reserved \n\n\n");    
