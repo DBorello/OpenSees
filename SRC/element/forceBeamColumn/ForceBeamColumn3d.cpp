4d9a08543fd930ffe73ea116d141cbe19c0195a7
mhscott <mhscott@0eff8f49-d3cf-49fd-9ff1-3c36910be21e>
2007-02-16 00:11:49 +0000

    adding references in source code
    git-svn-id: svn://peera.berkeley.edu/usr/local/svn/OpenSees/trunk@2757 0eff8f49-d3cf-49fd-9ff1-3c36910be21e

--- SRC/element/forceBeamColumn/ForceBeamColumn3d.cpp
+++ SRC/element/forceBeamColumn/ForceBeamColumn3d.cpp
@@ -18,9 +18,45 @@
 **                                                                    **
 ** ****************************************************************** */
                                                                         
-// $Revision: 1.24 $
-// $Date: 2007-02-02 04:11:49 $
+// $Revision: 1.25 $
+// $Date: 2007-02-16 00:11:49 $
 // $Source: /usr/local/cvs/OpenSees/SRC/element/forceBeamColumn/ForceBeamColumn3d.cpp,v $
+
+/*
+ * References
+ *
+
+State Determination Algorithm
+---
+Neuenhofer, A. and F. C. Filippou (1997). "Evaluation of Nonlinear Frame Finite
+Element Models." Journal of Structural Engineering, 123(7):958-966.
+
+Spacone, E., V. Ciampi, and F. C. Filippou (1996). "Mixed Formulation of
+Nonlinear Beam Finite Element." Computers and Structures, 58(1):71-83.
+
+
+Plastic Hinge Integration
+---
+Scott, M. H. and G. L. Fenves (2006). "Plastic Hinge Integration Methods for
+Force-Based Beam-Column Elements." Journal of Structural Engineering,
+132(2):244-252.
+
+
+Analytical Response Sensitivity (DDM)
+---
+Scott, M. H., P. Franchin, G. L. Fenves, and F. C. Filippou (2004).
+"Response Sensitivity for Nonlinear Beam-Column Elements."
+Journal of Structural Engineering, 130(9):1281-1288.
+
+
+Software Design
+---
+Scott, M. H., G. L. Fenves, F. T. McKenna, and F. C. Filippou (2007).
+"Software Patterns for Nonlinear Beam-Column Models."
+Journal of Structural Engineering, Approved for publication, February 2007.
+
+ *
+ */
 
 #include <math.h>
 #include <stdlib.h>
