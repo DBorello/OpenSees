e48ca2d8638dfabb8a55156d4ed9c1b1776d3f5c
fmk <fmk@0eff8f49-d3cf-49fd-9ff1-3c36910be21e>
2007-07-30 21:30:00 +0000

    a.neeman - changes for FileChannel class
    git-svn-id: svn://peera.berkeley.edu/usr/local/svn/OpenSees/trunk@3013 0eff8f49-d3cf-49fd-9ff1-3c36910be21e

--- SRC/nDarray/nDarray.h
+++ SRC/nDarray/nDarray.h
@@ -101,6 +101,8 @@ class nDarray_rep
 
     friend class Cosseratstresstensor;
     friend class Cosseratstraintensor;
+    friend class FileChannel;
+
 
   private:
     double *pd_nDdata;  // nD array as 1D array
@@ -138,6 +140,7 @@ class nDarray
 
     friend class Cosseratstresstensor;
     friend class Cosseratstraintensor;
+    friend class FileChannel;
 
 //.. no need    friend class GaussPoint;
           // explanation why this one should be a friend instead
