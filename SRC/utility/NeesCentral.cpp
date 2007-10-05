07f55d019dc8872d053bdcd510674ca83f5a4f4d
fmk <fmk@0eff8f49-d3cf-49fd-9ff1-3c36910be21e>
2007-10-05 22:07:08 +0000

    fmk - cleaning up memory, rewriting function to add a directory
    git-svn-id: svn://peera.berkeley.edu/usr/local/svn/OpenSees/trunk@3056 0eff8f49-d3cf-49fd-9ff1-3c36910be21e

--- SRC/utility/NeesCentral.cpp
+++ SRC/utility/NeesCentral.cpp
@@ -18,8 +18,8 @@
 **                                                                    **
 ** ****************************************************************** */
                                                                         
-// $Revision: 1.2 $
-// $Date: 2007-10-02 18:29:39 $
+// $Revision: 1.3 $
+// $Date: 2007-10-05 22:07:08 $
 // $Source: /usr/local/cvs/OpenSees/SRC/utility/NeesCentral.cpp,v $
                                                                         
                                                                         
@@ -34,6 +34,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
 
 #ifdef _HTTPS
 
@@ -227,57 +228,6 @@ neesGET(const char *page,
   return 0;  
 }
 
-
-
-#ifdef _WIN32
-int __cdecl
-#else
-int
-#endif
-neesSENDTrial(const char *cookie,
-	      int projID,
-	      int expID,
-	      const char *name,
-	      const char *title,
-	      const char *objective,
-	      const char *description)
-{
-  char *resHTML =0;
-  int trialID;
-
-  char neesPage[96];
-  sprintf(neesPage,"/REST/Project/%d/Experiment/%d/Trial%c",projID,expID,'\0');
-
-  char *xmlData = (char *)malloc(512+strlen(name)+strlen(title)+strlen(objective)+strlen(description));
-
-  //  char xmlData[3072];
-  sprintf(xmlData,"<?xml version=\"1.0\" encoding=\"UTF-8\"?> <central xmlns=\"http://central.nees.org/api\" xmlns:type=\"central\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:tns=\"http://central.nees.org/api\"> <Trial> <name>%s</name> <title>%s</title> <objective>%s</objective> <description>%s</description> </Trial> </central> %c",name, title, objective, description,'\0');
-
-  trialID = neesSEND(neesPage, cookie, xmlData, 0, true, &resHTML);
-
-  free(xmlData);
-
-  if (trialID == 0 && resHTML != 0) {    
-
-      char *startNextCookie = strstr(resHTML, neesPage);
-      
-      if (startNextCookie != NULL) {
-	startNextCookie += strlen(neesPage);
-	sscanf(startNextCookie, "/%d", &trialID);
-
-	
-      } else {
-	trialID = -1;
-      }
-      
-  } else
-    trialID = -1;
-  
-  if (resHTML != 0)
-    free(resHTML);
-  
-  return trialID;
-}
 #ifdef _WIN32
 int __cdecl
 #else
@@ -289,6 +239,7 @@ neesSEND_File(const char *page,
 	      const char *contentType,
 	      bool doPOST,
 	      char **res) {
+  
 
   char *URL="central.nees.org";
 
@@ -326,6 +277,60 @@ neesSEND_File(const char *page,
 }
 
 
+#ifdef _WIN32
+int __cdecl
+#else
+int
+#endif
+neesADD_Trial(const char *cookie,
+	      int projID,
+	      int expID,
+	      const char *name,
+	      const char *title,
+	      const char *objective,
+	      const char *description)
+{
+  char *resHTML =0;
+  int trialID;
+
+  char neesPage[96];
+  sprintf(neesPage,"/REST/Project/%d/Experiment/%d/Trial%c",projID,expID,'\0');
+
+  char *xmlData = (char *)malloc(512+strlen(name)+strlen(title)+strlen(objective)+strlen(description));
+
+  sprintf(xmlData,"<?xml version=\"1.0\" encoding=\"UTF-8\"?> <central xmlns=\"http://central.nees.org/api\" xmlns:type=\"central\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:tns=\"http://central.nees.org/api\"> <Trial> <name>%s</name> <title>%s</title> <objective>%s</objective> <description>%s</description> </Trial> </central> %c",name, title, objective, description,'\0');
+
+  trialID = neesSEND(neesPage, cookie, xmlData, 0, true, &resHTML);
+
+
+
+  if (trialID == 0 && resHTML != 0) {    
+
+      char *startNextCookie = strstr(resHTML, neesPage);
+      
+      if (startNextCookie != NULL) {
+	startNextCookie += strlen(neesPage);
+	sscanf(startNextCookie, "/%d", &trialID);
+	
+      } else {
+	trialID = -1;
+      }
+      
+  } else
+    trialID = -1;
+
+  //
+  // clean up memory
+  //
+
+  free(xmlData);
+  if (resHTML != 0)
+    free(resHTML);
+
+  
+  return trialID;
+}
+
 
 
 
@@ -334,23 +339,21 @@ int __cdecl
 #else
 int
 #endif
-neesPOSTTrial_File(const char *cookie,
-		   int projID,
-		   int expID,
-		   const char *filename)
+neesADD_Trial_XmlFromFromFile(const char *cookie,
+			      int projID,
+			      int expID,
+			      const char *filename)
 {
   char *resHTML =0;
   int trialID;
-
+  
   char neesPage[96];
   sprintf(neesPage,"/REST/Project/%d/Experiment/%d/Trial%c",projID,expID,'\0');
-
+  
   trialID = neesSEND_File(neesPage, cookie, filename, 0, true, &resHTML);
 
   if (trialID == 0 && resHTML != 0) {    
     
-      fprintf(stderr,"\n\nRETURNED: %s\n", resHTML);
-
       char *startNextCookie = strstr(resHTML, neesPage);
       
       if (startNextCookie != NULL) {
@@ -382,6 +385,7 @@ neesADD_TrialAnalysisFile(const char *cookie,
 			  int expID,
 			  int trialID,
 			  const char *path,
+			  int basePathLength,
 			  const char *filename,
 			  const char *description)
 {
@@ -404,6 +408,10 @@ neesADD_TrialAnalysisFile(const char *cookie,
     return -1;
   }
 
+  //
+  // we get information about where the path for analysis part of trial is
+  // and we can create the page address which contains this path, the provided path and filename
+  //
 
   char *startData = strstr(resHTML, "DataFile");
   startData+=15; // DataFile link="
@@ -428,7 +436,7 @@ neesADD_TrialAnalysisFile(const char *cookie,
   char *neesFilePage = (char *)malloc(endData-startData+10+strlen(path)+strlen(filename)); // 10 for /content
   strncpy(neesFilePage, startData, endData-startData);
   strncpy(&neesFilePage[endData-startData], "\0", 1);
-  strcat(neesFilePage,path);
+  strcat(neesFilePage,&path[basePathLength]);
   if (neesFilePage[strlen(neesFilePage)-1] != '/')
     strcat(neesFilePage,"/");
   strcat(neesFilePage,filename);
@@ -436,12 +444,13 @@ neesADD_TrialAnalysisFile(const char *cookie,
   free(resHTML);
 
   //
-  // once we have file name we can do post meta data
+  // we can create meta data for file, to be sent to page in a POST
   //
 
-  char *xmlData = (char *)malloc(512+strlen(filename)+strlen(description));  
+  char *xmlData = (char *)malloc(512+strlen(filename)+strlen(description)+strlen(path));  
 
   sprintf(xmlData,"<?xml version=\"1.0\" encoding=\"UTF-8\"?> <central xmlns=\"http://central.nees.org/api\" xmlns:type=\"central\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:tns=\"http://central.nees.org/api\"> <DataFile> <name>%s</name> <Description>%s</Description> </DataFile> </central> %c",filename, description,'\0');
+
 
   res = neesSEND(neesFilePage, cookie, xmlData, 0, true, &resHTML);
 
@@ -454,17 +463,25 @@ neesADD_TrialAnalysisFile(const char *cookie,
   free(resHTML);
 
   //
-  // once we have file name we can do a PUT of actual data
+  // once we have POSTED file xml data we can do a PUT of actual data, add /content to page
+  // placing local location of file in xmlData array
   //
 
   strcat(neesFilePage,"/content");
-  res = neesSEND_File(neesFilePage, cookie, filename, 0, false, &resHTML);
+  strcpy(xmlData, path);
+  strcat(xmlData,filename);
+
+  res = neesSEND_File(neesFilePage, cookie, xmlData, 0, false, &resHTML);
 
   if (res < 0) {
     fprintf(stderr, "ERROR neesADD_TrialAnalysisFile - failed to send file contents\n");    
     free(resHTML);
     return -1;
   }
+
+  //
+  // clean up memory
+  //
 
   free(neesFilePage);
   free(xmlData);
@@ -476,6 +493,125 @@ neesADD_TrialAnalysisFile(const char *cookie,
 }
 
 
+int neesADD_TrialAnalysisDir(const char *cookie,
+			     int projID,
+			     int expID,
+			     int trialID,
+			     const char *path,
+			     const char *dirName)
+{
+
+  char *resHTML =0;
+  int res;
+
+  //
+  // we first need to get the projects experiments trial details
+  // this includes information  about where files are stored
+  //
+
+  char neesPage[96];
+  sprintf(neesPage,"/REST/Project/%d/Experiment/%d/Trial/%d%c",projID,expID,trialID,'\0');
+
+  res = neesGET(neesPage, cookie, &resHTML);
+
+  if (res < 0) {
+    fprintf(stderr, "ERROR neesADD_TrialAnalysisFile - failed to find Trial\n");    
+    free(resHTML);
+    return -1;
+  }
+
+  //
+  // we get information about where the path for analysis part of trial is
+  // and we can create the page address which contains this path, the provided path and filename
+  //
+
+  char *startData = strstr(resHTML, "DataFile");
+  startData+=15; // DataFile link="
+
+  char *endData = strstr(resHTML,"Analysis");
+  endData+=8;
+
+  if (startData == 0 || endData == 0) {
+    fprintf(stderr, "ERROR neesADD_TrialAnalysisFile - failed to get Analysis dir in Trial\n");    
+    free(resHTML);
+    return -1;
+  }
+
+  char *nextStartData = startData+1;
+  while (nextStartData != 0 && nextStartData < endData) {
+    nextStartData = strstr(nextStartData, "DataFile");
+    nextStartData+=15;
+    if (nextStartData < endData)
+      startData = nextStartData;
+  }
+
+  char *neesFilePage = (char *)malloc(endData-startData+10+strlen(path)+strlen(dirName)); // 10 for /content
+  strncpy(neesFilePage, startData, endData-startData);
+  strncpy(&neesFilePage[endData-startData], "\0", 1);
+  strcat(neesFilePage,path);
+  if (neesFilePage[strlen(neesFilePage)-1] != '/')
+    strcat(neesFilePage,"/");
+
+  char *restPath = (char *)malloc(strlen(neesFilePage));
+  strcpy(restPath,&neesFilePage[11]);
+  strcat(&restPath[strlen(restPath)-2],"");  
+
+  strcat(neesFilePage,dirName);
+
+  free(resHTML);
+
+  //
+  // we can create meta data for file, to be sent to page in a POST
+  //
+
+  char *contentLink = (char *)malloc(strlen(neesFilePage)+10); 
+  strcpy(contentLink, neesFilePage);
+  strcat(contentLink,"/content");
+
+  char *xmlData = (char *)malloc(512+strlen(dirName)+strlen(neesFilePage)+strlen(contentLink));  
+
+  sprintf(xmlData,"<?xml version=\"1.0\" encoding=\"UTF-8\"?> <central xmlns=\"http://central.nees.org/api\" xmlns:type=\"central\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:tns=\"http://central.nees.org/api\"> <DataFile isDirectory=\"true\"> <name>%s</name> <path>%s</path> <contentLink>%s</contentLink></DataFile> </central> %c",dirName, restPath, contentLink, '\0');
+
+  res = neesSEND(neesFilePage, cookie, xmlData, 0, true, &resHTML);
+
+
+  if (res < 0) {
+    fprintf(stderr, "IGNORE neesSEND error - currently a bug in central.nees.org nees\n");    
+    //    free(resHTML);
+    //    return -1;
+  }
+
+
+  free(resHTML);
+
+  //
+  // once we have file name we can do a PUT with blank data
+  //
+
+
+  res = neesSEND(neesFilePage, cookie, "", 0, false, &resHTML);
+
+  if (res < 0) {
+    fprintf(stderr, "ERROR neesADD_TrialAnalysisFile - failed to send file contents\n");    
+    free(resHTML);
+    return -1;
+  }
+
+  //
+  // clean up memory
+  //
+
+  free(restPath);
+  free(neesFilePage);
+  free(xmlData);
+  free(contentLink);
+  if (resHTML != 0)
+    free(resHTML);
+
+  return 0;
+}
+
+/*
 int neesADD_TrialAnalysisDir(const char *cookie,
 			     int projID,
 			     int expID,
@@ -510,28 +646,47 @@ int neesADD_TrialAnalysisDir(const char *cookie,
 
   startData = strstr(resHTML, "DataFile");
   startData+=20; // <DataFile link="/REST
-  
+
   endData = strstr(resHTML,"Analysis");
   endData+=8;
   
-  char *neesFilePage = (char *)malloc(endData-startData+2); 
+  char *neesFilePage = new char[endData-startData+1];
+
+
   strncpy(neesFilePage, startData, endData-startData);
   strncpy(&neesFilePage[endData-startData], "\0", 1);
 
+
+  //char *expData = strstr(startData,"Experiment");
+  //  char *trialData = strstr(startData,"/Trial");
+  //strncpy(neesFilePage, startData, expData-startData);
+  //strncpy(&neesFilePage[expData-startData], trialData, endData-trialData);
+  //strncpy(&neesFilePage[expData-startData+endData-trialData+1], "\0", 1);
+
   free(resHTML);
+
+  int dataLength = 512+strlen(neesFilePage)+strlen(path);
 
   //
   // now we can create the directory
   //
+
+  char *neesBrowserPage =  new char[dataLength];
   
-  char *neesBrowserPage = (char *)malloc(256+strlen(neesFilePage)+strlen(path));;
-  sprintf(neesBrowserPage,"?projid=%d&expid=%d&trialid=%d&tloc=Analysis&section=&view=browser&basepath=%s&path=%s&floc=Mkdir",
+  //  sprintf(neesBrowserPage,"?projid=%d&expid=%d&trialid=%d&tloc=Analysis&section=&view=browser&basepath=%s&path=%s&floc=Mkdir",
+  sprintf(neesBrowserPage,"?projid=%d&expid=%d&trialid=%d&action=DisplayTrialAnalysis&view=browser&basepath=%s&path=%s&floc=Mkdir",
 	  projID, expID, trialID, neesFilePage, path);
 
-  char *data = (char *)malloc(256+strlen(neesFilePage)+strlen(path)+strlen(dirName));;
 
-  sprintf(data,"projid=%d&expid=%d&trialid=%d&tloc=Analysis&section=&view=browser&basepath=%s&path=%s&doit=1&newdir=%s",
+  dataLength+= strlen(dirName);
+
+  char *data = new char[dataLength];
+  sprintf(data,"projid=%d&expid=%d&trialid=%d&action=DisplayTrialAnalysis&view=browser&basepath=%s&path=%s&doit=1&newdir=%s",
 	  projID, expID, trialID, &neesFilePage[5], path, dirName); //&neesFilePage[5] to remove /File 
+
+
+//  sprintf(data,"projid=%d&expid=%d&trialid=%d&action=DisplayTrialAnalysis&view=browser&basepath=%s&path=%s&doit=1&newdir=%s",
+//	  projID, expID, trialID, &neesFilePage[5], path, dirName); //&neesFilePage[5] to remove /File 
 
   char *contentType="application/x-www-form-urlencoded";
 
@@ -543,13 +698,14 @@ int neesADD_TrialAnalysisDir(const char *cookie,
   }
 
   // clean up memory
-  free(data);
-  free(neesBrowserPage);
-  free(neesFilePage);
+  delete [] data;
+  delete [] neesBrowserPage;
+  delete [] neesFilePage;
   free(resHTML);
 
   return 0;
 }
+*/
 
 #endif
 
