f12bb80aa14b9b95687a9580b0b9cac8dcbf37a1
fmk <fmk@0eff8f49-d3cf-49fd-9ff1-3c36910be21e>
2007-11-07 23:55:26 +0000

    fmk - another test
    git-svn-id: svn://peera.berkeley.edu/usr/local/svn/OpenSees/trunk@3224 0eff8f49-d3cf-49fd-9ff1-3c36910be21e

--- SRC/OPS_Globals.h
+++ SRC/OPS_Globals.h
@@ -21,8 +21,8 @@
 **                                                                    **
 ** ****************************************************************** */
                                                                         
-// $Revision: 1.17 $
-// $Date: 2007-11-07 23:53:06 $
+// $Revision: 1.18 $
+// $Date: 2007-11-07 23:55:26 $
 // $Source: /usr/local/cvs/OpenSees/SRC/OPS_Globals.h,v $
                                                                         
 // Written: fmk 
@@ -31,7 +31,7 @@
 // Description: This file contains global variables used in OpenSees files.
 // if you change some of the variables, you must recompile ALL the code.
 
-#define testCVS TEST2
+#define testCVS TEST3
 
 #define OPS_VERSION "1.7.4"
 
