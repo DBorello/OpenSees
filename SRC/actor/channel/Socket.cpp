1db873070373449f52953fa6c07bd0c66a9d05b0
fmk <fmk@0eff8f49-d3cf-49fd-9ff1-3c36910be21e>
2007-11-29 19:00:51 +0000

    a.achellenberg - bug fix
    git-svn-id: svn://peera.berkeley.edu/usr/local/svn/OpenSees/trunk@3249 0eff8f49-d3cf-49fd-9ff1-3c36910be21e

--- SRC/actor/channel/Socket.cpp
+++ SRC/actor/channel/Socket.cpp
@@ -17,13 +17,14 @@
 **   Filip C. Filippou (filippou@ce.berkeley.edu)                     **
 **                                                                    **
 ** ****************************************************************** */
-                                                                        
-// $Revision: 1.2 $
-// $Date: 2006-12-15 00:12:32 $
+
+// $Revision: 1.3 $
+// $Date: 2007-11-29 19:00:51 $
 // $Source: /usr/local/cvs/OpenSees/SRC/actor/channel/Socket.cpp,v $
-                                                                        
-// Written: fmk 11/06
-// Revised:
+
+// Written: fmk
+// Created: 11/06
+// Revision: A
 //
 // Purpose: This file contains functions needed in socket calls
 
@@ -37,14 +38,14 @@ extern "C" {
 #include <winsock2.h>
 };
 #else
-  #include <sys/socket.h>
-  #include <sys/types.h>
-  #include <netinet/in.h>
-  #include <netinet/tcp.h>
-  #include <arpa/inet.h>
-  #include <netdb.h>
-  #include <unistd.h>
-  #include <strings.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <netinet/tcp.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <unistd.h>
+#include <strings.h>
 #endif
 
 
@@ -52,40 +53,40 @@ extern "C" {
 #define MAX_INET_ADDR 28
 
 #ifdef _WIN32
-  typedef SOCKET socket_type;
-  typedef int socklen_type;
-  static int numSockets = 0;
+typedef SOCKET socket_type;
+typedef int socklen_type;
+static int numSockets = 0;
 #else
-  typedef int socket_type;
-  typedef socklen_t socklen_type;
+typedef int socket_type;
+typedef socklen_t socklen_type;
 #endif
 
 
 int
 startup_sockets(void)
 {
-  #ifdef _WIN32
-  
-  WSADATA wsaData;
-  if (numSockets == 0) {
-    numSockets++;
-    return WSAStartup(0x0002, &wsaData) == 0;
-  } else
+#ifdef _WIN32
+    WSADATA wsaData;
+    if (numSockets == 0) {
+        numSockets++;
+        return WSAStartup(0x0002, &wsaData) == 0;
+    } else {
+        numSockets++;
+        return 1;
+    }
+#else
     return 1;
-  #else
-    return 1;
-  #endif
+#endif
 } 
 
 
-void cleanup_sockets(void)
+void
+cleanup_sockets(void)
 {
-  #ifdef _WIN32
-  numSockets--;
-  if (numSockets == 0) {
-    WSACleanup();
-  }
-  #endif
+#ifdef _WIN32
+    numSockets--;
+    if (numSockets == 0) {
+        WSACleanup();
+    }
+#endif
 }
-
-
