3dce747766bbbd54fb18eadad2f86663eb78dd84
fmk <fmk@0eff8f49-d3cf-49fd-9ff1-3c36910be21e>
2008-03-05 19:13:56 +0000

    fmk - adding MPI_ANY_SOURCE to possible pid in recv and some compile fixes
    git-svn-id: svn://peera.berkeley.edu/usr/local/svn/OpenSees/trunk@3335 0eff8f49-d3cf-49fd-9ff1-3c36910be21e

--- SRC/tcl/commands.cpp
+++ SRC/tcl/commands.cpp
@@ -18,8 +18,8 @@
 **                                                                    **
 ** ****************************************************************** */
                                                                         
-// $Revision: 1.110 $
-// $Date: 2008-03-04 23:48:48 $
+// $Revision: 1.111 $
+// $Date: 2008-03-05 19:13:56 $
 // $Source: /usr/local/cvs/OpenSees/SRC/tcl/commands.cpp,v $
                                                                         
                                                                         
@@ -5326,9 +5326,8 @@ getNP(ClientData clientData, Tcl_Interp *interp, int argc, TCL_Char **argv)
 int 
 opsBarrier(ClientData clientData, Tcl_Interp *interp, int argc, TCL_Char **argv)
 {
-  int pid = 0;
 #ifdef _PARALLEL_INTERPRETERS
-  return MPI_Barrier();
+  return MPI_Barrier(MPI_COMM_WORLD);
 #endif
 
   return TCL_OK;  
@@ -5340,7 +5339,7 @@ opsSend(ClientData clientData, Tcl_Interp *interp, int argc, TCL_Char **argv)
 {
   int pid = 0;
 #ifdef _PARALLEL_INTERPRETERS
-  if (argv < 2)
+  if (argc < 2)
     return TCL_OK;
 
   int otherPID = -1;
@@ -5348,8 +5347,11 @@ opsSend(ClientData clientData, Tcl_Interp *interp, int argc, TCL_Char **argv)
   int np =  theMachineBroker->getNP();
   const char *dataToSend = argv[argc-1];
   int msgLength = strlen(dataToSend)+1;
-
-  if (strcmp(argv[1], "-pid") == 0 && argv > 3) {
+  
+  const char *gMsg = dataToSend;
+  //  strcpy(gMsg, dataToSend);
+  
+  if (strcmp(argv[1], "-pid") == 0 && argc > 3) {
 
     if (Tcl_GetInt(interp, argv[2], &otherPID) != TCL_OK) {
       opserr << "send -pid pid? data? - pid: " << argv[2] << " invalid\n";
@@ -5358,11 +5360,8 @@ opsSend(ClientData clientData, Tcl_Interp *interp, int argc, TCL_Char **argv)
 
     if (otherPID > -1 && otherPID != myPID && otherPID < np) {
       
-      char *gMsg = new char [msgLength];
-      strcpy(dataToSend, gMsg);
-      
       MPI_Send((void *)(&msgLength), 1, MPI_INT, otherPID, 0, MPI_COMM_WORLD);
-      MPI_Send((void *)gMsg, msgLength, MPI_CHAR, otherPID, 0, MPI_COMM_WORLD);
+      MPI_Send((void *)gMsg, msgLength, MPI_CHAR, otherPID, 1, MPI_COMM_WORLD);
 
     } else {
       opserr << "send -pid pid? data? - pid: " << otherPID << " invalid\n";
@@ -5371,8 +5370,8 @@ opsSend(ClientData clientData, Tcl_Interp *interp, int argc, TCL_Char **argv)
 
   } else {
     if (pid == 0) {
-      MPI_BCAST((void *)(&msgLength), 1, MPI_INT,  myPID, MPI_COMM_WORLD);
-      MPI_BCAST((void *)gMsg, msgLength, MPI_CHAR, myPID, 0, MPI_COMM_WORLD);
+      MPI_Bcast((void *)(&msgLength), 1, MPI_INT,  0, MPI_COMM_WORLD);
+      MPI_Bcast((void *)gMsg, msgLength, MPI_CHAR, 0, MPI_COMM_WORLD);
     } else {
       opserr << "send data - only process 0 can do a broadcast - you may need to kill the application";
       return TCL_ERROR;
@@ -5390,30 +5389,50 @@ opsRecv(ClientData clientData, Tcl_Interp *interp, int argc, TCL_Char **argv)
 {
   int pid = 0;
 #ifdef _PARALLEL_INTERPRETERS
-  if (argv < 2)
+  if (argc < 2)
     return TCL_OK;
 
   int otherPID = 0;
   int myPID =  theMachineBroker->getPID();
   int np =  theMachineBroker->getNP();
   const char *varToSet = argv[argc-1];
-  if (strcmp(argv[1], "-pid") == 0 && argv > 3) {
 
-    if (Tcl_GetInt(interp, argv[2], &otherPID) != TCL_OK) {
-      opserr << "recv -pid pid? data? - pid: " << argv[2] << " invalid\n";
-      return TCL_ERROR;
+  int msgLength = 0;
+  char *gMsg = 0;
+
+  if (strcmp(argv[1], "-pid") == 0 && argc > 3) {
+
+    bool fromAny = false;
+
+    if ((strcmp(argv[2], "ANY") == 0) || (strcmp(argv[2],"ANY_SOURCE") == 0) ||
+	(strcmp(argv[2], "MPI_ANY_SOURCE") == 0)) {
+      fromAny = true;
+    } else {
+      if (Tcl_GetInt(interp, argv[2], &otherPID) != TCL_OK) {
+	opserr << "recv -pid pid? data? - pid: " << argv[2] << " invalid\n";
+	return TCL_ERROR;
+      }
     }
 
     if (otherPID > -1 && otherPID != pid && otherPID < np) {
+      MPI_Status status;
+      
+      if (fromAny = false)
+	MPI_Recv((void *)(&msgLength), 1, MPI_INT, otherPID, 0, MPI_COMM_WORLD, &status);
+      else
+	MPI_Recv((void *)(&msgLength), 1, MPI_INT, MPI_ANY_SOURCE, 0, MPI_COMM_WORLD, &status);
 
-      int msgLength;
-      MPI_Recv((void *)(&msgLength), 1, MPI_INT, otherPID, 0, MPI_COMM_WORLD);
 
-      char *gMsg = new char [msgLength];
+      if (msgLength > 0) {
+	gMsg = new char [msgLength];
 
-      MPI_Recv((void *)gMsg, msgLength, MPI_CHAR, otherPID, 0, MPI_COMM_WORLD);
+	if (fromAny = false)
+	  MPI_Recv((void *)gMsg, msgLength, MPI_CHAR, otherPID, 1, MPI_COMM_WORLD, &status);
+	else
+	  MPI_Recv((void *)gMsg, msgLength, MPI_CHAR, MPI_ANY_SOURCE, 1, MPI_COMM_WORLD, &status);
 
-      Tcl_SetVar(interp, varToSet, gMsg, TCL_LEAVE_ERR_MSG);
+	Tcl_SetVar(interp, varToSet, gMsg, TCL_LEAVE_ERR_MSG);
+      }
 
     } else {
       opserr << "send -pid pid? data? - " << otherPID << " invalid\n";
@@ -5422,8 +5441,16 @@ opsRecv(ClientData clientData, Tcl_Interp *interp, int argc, TCL_Char **argv)
   } else {
 
     if (myPID != 0) {
-      MPI_BCAST((void *)(&msgLength), 1, MPI_INT, 0, MPI_COMM_WORLD);
-      MPI_BCAST((void *)gMsg, msgLength, MPI_CHAR, 0, MPI_COMM_WORLD);
+      MPI_Bcast((void *)(&msgLength), 1, MPI_INT, 0, MPI_COMM_WORLD);
+
+      if (msgLength > 0) {
+	gMsg = new char [msgLength];
+
+	MPI_Bcast((void *)gMsg, msgLength, MPI_CHAR, 0, MPI_COMM_WORLD);
+
+	Tcl_SetVar(interp, varToSet, gMsg, TCL_LEAVE_ERR_MSG);
+      }
+
     } else {
       opserr << "send data - only process 0 can do a broadcast - you may need to kill the application";
       return TCL_ERROR;
