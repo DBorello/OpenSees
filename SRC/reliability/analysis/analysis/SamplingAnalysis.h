951bfb8fc6f87ada8ac9121edca4866fc5b27df6
fmk <fmk@0eff8f49-d3cf-49fd-9ff1-3c36910be21e>
2008-03-13 22:20:34 +0000

    fmk - making SamplingAnalysis an abstract class, replaced with ImportnaceSamplingAnalysis
    git-svn-id: svn://peera.berkeley.edu/usr/local/svn/OpenSees/trunk@3349 0eff8f49-d3cf-49fd-9ff1-3c36910be21e

--- SRC/reliability/analysis/analysis/SamplingAnalysis.h
+++ SRC/reliability/analysis/analysis/SamplingAnalysis.h
@@ -1,85 +1,37 @@
-/* ****************************************************************** **
-**    OpenSees - Open System for Earthquake Engineering Simulation    **
-**          Pacific Earthquake Engineering Research Center            **
-**                                                                    **
-**                                                                    **
-** (C) Copyright 2001, The Regents of the University of California    **
-** All Rights Reserved.                                               **
-**                                                                    **
-** Commercial use of this program without express permission of the   **
-** University of California, Berkeley, is strictly prohibited.  See   **
-** file 'COPYRIGHT'  in main directory for information on usage and   **
-** redistribution,  and for a DISCLAIMER OF ALL WARRANTIES.           **
-**                                                                    **
-** Developed by:                                                      **
-**   Frank McKenna (fmckenna@ce.berkeley.edu)                         **
-**   Gregory L. Fenves (fenves@ce.berkeley.edu)                       **
-**   Filip C. Filippou (filippou@ce.berkeley.edu)                     **
-**                                                                    **
-** Reliability module developed by:                                   **
-**   Terje Haukaas (haukaas@ce.berkeley.edu)                          **
-**   Armen Der Kiureghian (adk@ce.berkeley.edu)                       **
-**                                                                    **
-** ****************************************************************** */
-                                                                        
-// $Revision: 1.5 $
-// $Date: 2007-10-25 16:49:13 $
-// $Source: /usr/local/cvs/OpenSees/SRC/reliability/analysis/analysis/SamplingAnalysis.h,v $
-
-
+// SamplingAnalysis.h: interface for the SamplingAnalysis class.
 //
-// Written by Terje Haukaas (haukaas@ce.berkeley.edu)
-//
+//////////////////////////////////////////////////////////////////////
 
-#ifndef SamplingAnalysis_h
-#define SamplingAnalysis_h
+#if !defined   SAMPLINGANALYSIS_H_100 
+#define        SAMPLINGANALYSIS_H_100
 
-#include <ReliabilityAnalysis.h>
-#include <ReliabilityDomain.h>
-#include <ProbabilityTransformation.h>
-#include <RandomNumberGenerator.h>
-#include <GFunEvaluator.h>
 
-#include <fstream>
-#include <tcl.h>
-using std::ofstream;
+#include "ReliabilityAnalysis.h"
 
-class SamplingAnalysis : public ReliabilityAnalysis
+class SamplingAnalysis : public ReliabilityAnalysis  
 {
-
 public:
-	SamplingAnalysis(	ReliabilityDomain *passedReliabilityDomain,
-						ProbabilityTransformation *passedProbabilityTransformation,
-						GFunEvaluator *passedGFunEvaluator,
-						RandomNumberGenerator *passedRandomNumberGenerator,
-						Tcl_Interp *passedInterp,
-						int passedNumberOfSimulations,
-						double passedTargetCOV,
-						double samplingStdv,
-						int printFlag,
-						TCL_Char *fileName,
-						Vector *startPoint,
-						int analysisTypeTag);
+	virtual bool getContribution();
+	virtual double getProbability();
+	virtual double getCov();
+	virtual int getNumOfSimulations();
 
-	~SamplingAnalysis();
+	virtual int analyze(void) =0;
 
-	int analyze(void);
+	virtual double getSampledValue(double)=0;
+	SamplingAnalysis();
+	virtual ~SamplingAnalysis();
 
 protected:
-	
+	double maxNumOfIterations;
+	int numOfSimulations;
+	double cov;
+	double probability;
+
 private:
-	ReliabilityDomain *theReliabilityDomain;
-	ProbabilityTransformation *theProbabilityTransformation;
-	GFunEvaluator *theGFunEvaluator;
-	RandomNumberGenerator *theRandomNumberGenerator;
-	Tcl_Interp *interp;
-	int numberOfSimulations;
-	double targetCOV;
-	double samplingStdv;
-	int printFlag;
-	char fileName[256];
-	Vector *startPoint;
-	int analysisTypeTag;
+
+
+
 };
 
-#endif
+#endif //  
