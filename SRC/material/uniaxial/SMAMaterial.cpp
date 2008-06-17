005844073d335633b5bc068e5d974af039c7d33a
fmk <fmk@0eff8f49-d3cf-49fd-9ff1-3c36910be21e>
2008-06-17 19:28:30 +0000

    D.Fugazza - update
    git-svn-id: svn://peera.berkeley.edu/usr/local/svn/OpenSees/trunk@3538 0eff8f49-d3cf-49fd-9ff1-3c36910be21e

--- SRC/material/uniaxial/SMAMaterial.cpp
+++ SRC/material/uniaxial/SMAMaterial.cpp
@@ -18,12 +18,6 @@
 **                                                                    **
 ** ****************************************************************** */
 
-// $Revison
-// $Date
-// $Source
-
-// written by: Davide Fugazza
-
 // Description: This file contains the class implementation for SMAMaterial.
 
 #include <SMAMaterial.h> 
@@ -32,10 +26,10 @@
 #include <math.h>
 
 SMAMaterial::SMAMaterial(int tag, double e, double el, double s_as_s, double s_as_f, 
-			 double s_sa_s, double s_sa_f)
+					                                   double s_sa_s, double s_sa_f)
   :UniaxialMaterial(tag,MAT_TAG_SMA), 
-   E(e), eps_L(el), sig_AS_s(s_as_s), sig_AS_f(s_as_f), sig_SA_s(s_sa_s), sig_SA_f(s_sa_f), Tcsi(0.0),
-   Tstrain(0.0), Tstress(0.0), Ttangent(0.0)
+   E(e), eps_L(el), sig_AS_s(s_as_s), sig_AS_f(s_as_f), sig_SA_s(s_sa_s), sig_SA_f(s_sa_f), 
+   Tstrain(0.0), Tstress(0.0), Ttangent(0.0), Tcsi(0.0)
 {
    // Initialize varibles
 
@@ -44,8 +38,8 @@ SMAMaterial::SMAMaterial(int tag, double e, double el, double s_as_s, double s_a
 
 SMAMaterial::SMAMaterial()
   :UniaxialMaterial(0,MAT_TAG_SMA), 
-   E(0.0), eps_L(0.0), sig_AS_s(0.0), sig_AS_f(0.0), sig_SA_s(0.0), sig_SA_f(0.0), Tcsi(0.0),
-   Tstrain(0.0), Tstress(0.0), Ttangent(0.0)
+   E(0.0), eps_L(0.0), sig_AS_s(0.0), sig_AS_f(0.0), sig_SA_s(0.0), sig_SA_f(0.0), 
+   Tstrain(0.0), Tstress(0.0), Ttangent(0.0), Tcsi(0.0)
 {
    // Initialize variables
 
@@ -61,321 +55,206 @@ int
 SMAMaterial::setTrialStrain(double strain, double strainRate)
 {
   
-///////////////////////////////////////////// 
- 
 Tstrain = strain;
 
-// Trick to avoid not-converged solutions
-
-//if (Tstrain*Cstrain <= 0) {
-
-//	 if (Tstrain > 0) {
-
-//	 Cstrain = 1.0e-9;
-
-//	 }
-
-//	 else {
-
-//   Cstrain = -1.0e-9;
-
-//	 }
-
-//Ccsi = 0;
-//Tstress = 0;
-//Cstress = E*Cstrain; 
-//Tcsi = 0;
-//Ttangent = 0;
-
-//}
-
-// If absolute value of current strain is <= 1.0e-9 (tolerance) then perform elastic step
-
-//if (fabs(Tstrain) <= 1.0e-9) {
-
-//	 Tcsi = 0.0;
-//	 Tstress = E*Tstrain;
-//	 Ttangent = E;
-
-//}
-	
- // Statements written to avoid defining for each quadrant " >= " or " <= " when 
- // evaluating the sign of (Tstrain - Cstrain)
-
-//if (fabs(Tstrain - Cstrain) <= 1.0e-9) {
-
-//	 if (Tstrain < 0) {
-
-//	 Tcsi = Ccsi;
-//	 Tstress = E*(Tstrain + Tcsi*eps_L); 
-//	 Ttangent = E;
-  
-//	 }
-
-//	 else {
-
-//    Tcsi = Ccsi;
-//	 Tstress = E*(Tstrain - Tcsi*eps_L); 
-//	 Ttangent = E;
- 
-//	 }
-   
-//}
-
 //-------------------------------------------------------------------------------------
-// FIRST QUADRANT (sigma > 0 and epsilon > 0): transformation A ---> S (loading)
+// A ---> S transformation (loading), sigma > 0 and epsilon > 0
 //-------------------------------------------------------------------------------------
 
 if ((Tstrain - Cstrain) > 0 && Tstrain > 0) {  
 
-         double eps_AS_s = sig_AS_s/E + Ccsi*eps_L;
-	 double eps_AS_f = sig_AS_f/E +      eps_L;
+     double eps_AS_s = sig_AS_s/E + Ccsi*eps_L;
+     double eps_AS_f = sig_AS_f/E +      eps_L;
 
 	 if (Tstrain <= eps_AS_s) {
 
-	 Tcsi = Ccsi;
-	 Tstress = E*(Tstrain - Tcsi*eps_L);
-	 Ttangent = E;
+	     Tcsi = Ccsi;
+	     Tstress = E*(Tstrain - Tcsi*eps_L);
+	     Ttangent = E;
 
-         }
+       }
 
-	 else  if (Tstrain > eps_AS_s && Tstrain < eps_AS_f) { 
+	 else if (Tstrain > eps_AS_s && Tstrain < eps_AS_f) { 
 
-		   if (Cstress <= sig_AS_s) {
+		if (Cstress <= sig_AS_s) {
 
-		   Tcsi = (Ccsi*E*Tstrain - Ccsi*sig_AS_f - E*Tstrain + sig_AS_s)/(-sig_AS_f + Ccsi*E*eps_L - E*eps_L + sig_AS_s);
-		   Tstress = E*(Tstrain - Tcsi*eps_L);
-		   double H = -(1 - Ccsi)*E/((1 - Ccsi)*(-E*eps_L) + sig_AS_s - sig_AS_f);
-		   Ttangent = E*(1 - eps_L*H);
+		    Tcsi = (Ccsi*E*Tstrain - Ccsi*sig_AS_f - E*Tstrain + sig_AS_s)/(-sig_AS_f + Ccsi*E*eps_L - E*eps_L + sig_AS_s);
+	          Tstress = E*(Tstrain - Tcsi*eps_L);
+		    double H = -(1 - Ccsi)*E/((1 - Ccsi)*(-E*eps_L) + sig_AS_s - sig_AS_f);
+		    Ttangent = E*(1 - eps_L*H);
 
-		   }
+		}
 
-		   else {
+		else {
 
-	           Tcsi = (Ccsi*E*Tstrain - Ccsi*sig_AS_f - E*Tstrain + Cstress)/(-sig_AS_f + Ccsi*E*eps_L - E*eps_L + Cstress);
-                   Tstress = E*(Tstrain - Tcsi*eps_L);
-                   double H = -(1 - Ccsi)*E/((1 - Ccsi)*(-E*eps_L) + Cstress - sig_AS_f);
-                   Ttangent = E*(1 - eps_L*H);
+	          Tcsi = (Ccsi*E*Tstrain - Ccsi*sig_AS_f - E*Tstrain + Cstress)/(-sig_AS_f + Ccsi*E*eps_L - E*eps_L + Cstress);
+                Tstress = E*(Tstrain - Tcsi*eps_L);
+                double H = -(1 - Ccsi)*E/((1 - Ccsi)*(-E*eps_L) + Cstress - sig_AS_f);
+                Ttangent = E*(1 - eps_L*H);
          
-		   }
+		}
 
-         }
+       }
 
 	 else {
 
-         Tcsi = 1;
-	 Tstress = E*(Tstrain - Tcsi*eps_L);
-	 Ttangent = E;
+           Tcsi = 1;
+	     Tstress = E*(Tstrain - Tcsi*eps_L);
+	     Ttangent = E;
 
 	 }
 
 }
 
-//if ((Tstrain - Cstrain) > 0 && Tstrain < 0 ) { 
-//	Tcsi = 0.0 ;
-//	Tstress = 0.0 ;
-//	Ttangent = 0.0 ;
-//}
 //-------------------------------------------------------------------------------------
-// FIRST QUADRANT (sigma > 0 and epsilon > 0): transformation S ---> A (unloading)
+// S ---> A  transformation (unloading), sigma > 0 and epsilon > 0
 //-------------------------------------------------------------------------------------
 
 if ((Tstrain - Cstrain) < 0 && Tstrain > 0) { 
 
-//	Tcsi = 0.0 ;
-//	Tstress = 0.0 ;
-//	Ttangent = 0.0 ;
-         double eps_SA_s = sig_SA_s/E + Ccsi*eps_L;
-	 double eps_SA_f = sig_SA_f/E;
+     double eps_SA_s = sig_SA_s/E + Ccsi*eps_L;
+     double eps_SA_f = sig_SA_f/E;
 
 	 if (Tstrain >= eps_SA_s) {
 
-	 Tcsi = Ccsi;
-	 Tstress = E*(Tstrain - Tcsi*eps_L);
-	 Ttangent = E;
+	     Tcsi = Ccsi;
+	     Tstress = E*(Tstrain - Tcsi*eps_L);
+	     Ttangent = E;
 
-         }
+       }
 
-	 else  if (Tstrain < eps_SA_s && Tstrain >= eps_SA_f) { 
+	 else if (Tstrain < eps_SA_s && Tstrain >= eps_SA_f) { 
 
-		   if (Cstress > sig_SA_s) {
+		if (Cstress > sig_SA_s) {
 
-	  	   Tcsi = (Ccsi*E*Tstrain - Ccsi*sig_SA_f)/(-sig_SA_f + Ccsi*E*eps_L + sig_SA_s);
-		   Tstress = E*(Tstrain - Tcsi*eps_L);
-		   double H = (Ccsi*E)/(- Ccsi*(-E*eps_L) + sig_SA_s - sig_SA_f);
-		   Ttangent = E*(1 - eps_L*H);
+	  	    Tcsi = (Ccsi*E*Tstrain - Ccsi*sig_SA_f)/(-sig_SA_f + Ccsi*E*eps_L + sig_SA_s);
+		    Tstress = E*(Tstrain - Tcsi*eps_L);
+		    double H = (Ccsi*E)/(- Ccsi*(-E*eps_L) + sig_SA_s - sig_SA_f);
+		    Ttangent = E*(1 - eps_L*H);
 
-		   }
+		 }
 
-		   else {
+		 else {
 
-		  Tcsi = (Ccsi*E*Tstrain - Ccsi*sig_SA_f)/(-sig_SA_f + Ccsi*E*eps_L + Cstress);
-          Tstress = E*(Tstrain - Tcsi*eps_L);
-          double H = (Ccsi*E)/(-Ccsi*(-E*eps_L) + Cstress - sig_SA_f);
-          Ttangent = E*(1 - eps_L*H);
+		     Tcsi = (Ccsi*E*Tstrain - Ccsi*sig_SA_f)/(-sig_SA_f + Ccsi*E*eps_L + Cstress);
+                 Tstress = E*(Tstrain - Tcsi*eps_L);
+                 double H = (Ccsi*E)/(-Ccsi*(-E*eps_L) + Cstress - sig_SA_f);
+                 Ttangent = E*(1 - eps_L*H);
          
-		   }
+		 }
 
-         }
+       }
 
 	 else {
 
-         Tcsi = 0;
-	 Tstress = E*(Tstrain);
-	 Ttangent = E;
+           Tcsi = 0;
+	     Tstress = E*(Tstrain);
+	     Ttangent = E;
 
 	 }
 
 }
 
-//if ((Tstrain - Cstrain) < 0 && Tstrain > 0) { 
-
-//	Tcsi = 0;
-//	Tstress = E*(Tstrain);
-//	Ttangent = E;
-//}
 //-------------------------------------------------------------------------------------
-// THIRD QUADRANT (sigma < 0 and epsilon < 0): transformation S ---> A (loading)
+// S ---> A  transformation (loading), sigma < 0 and epsilon < 0
 //-------------------------------------------------------------------------------------
 
 if ((Tstrain - Cstrain) < 0 && Tstrain < 0.0) { 
 	
+     double eps_AS_s = -sig_AS_s/E - Ccsi*eps_L;
+     double eps_AS_f = -sig_AS_f/E - eps_L;
 
-//       double eps_AS_s = -sig_AS_s/E - Ccsi*eps_L ;
-//	 double eps_AS_f = -sig_AS_f/E - eps_L ;
+     if (Tstrain >= eps_AS_s) {
 
-//	 if (Tstrain >= eps_AS_s) {
+	   Tcsi = Ccsi;
+	   Tstress = E*(Tstrain + Tcsi*eps_L);
+	   Ttangent = E;
 
-//	 Tcsi = Ccsi;
-//	 Tstress = E*(Tstrain + Tcsi*eps_L);
-//	 Ttangent = E;
+      }
 
-//       }
+	else if (Tstrain < eps_AS_s && Tstrain > eps_AS_f) { 
 
-//	 else  if (Tstrain < eps_AS_s && Tstrain > eps_AS_f) { 
+	     if (Cstress >= -sig_AS_s) {
 
-//		   if (Cstress >= -sig_AS_s) {
+		   Tcsi = (Ccsi*E*Tstrain + Ccsi*sig_AS_f - E*Tstrain - sig_AS_s)/(sig_AS_f - Ccsi*E*eps_L + E*eps_L - sig_AS_s);
+		   Tstress = E*(Tstrain + Tcsi*eps_L);
+		   double H = (1 - Ccsi)*E/((1 - Ccsi)*(E*eps_L) - sig_AS_s + sig_AS_f);
+		   Ttangent = E*(1 - eps_L*H);
 
-//		   Tcsi = (Ccsi*E*Tstrain + Ccsi*sig_AS_f - E*Tstrain - sig_AS_s)/(sig_AS_f - Ccsi*E*eps_L + E*eps_L - sig_AS_s);
-//		   Tstress = E*(Tstrain + Tcsi*eps_L);
-//		   double H = (1 - Ccsi)*E/((1 - Ccsi)*(E*eps_L) - sig_AS_s + sig_AS_f);
-//		   Ttangent = E*(1 - eps_L*H);
+	     }
 
-//		   }
+	     else {
 
-//		   else {
-
-//                 Tcsi = (Ccsi*E*Tstrain + Ccsi*sig_AS_f - E*Tstrain + Cstress)/(sig_AS_f - Ccsi*E*eps_L + E*eps_L + Cstress);
-//                 Tstress = E*(Tstrain + Tcsi*eps_L);
-//                 double H = (1 - Ccsi)*E/((1 - Ccsi)*(E*eps_L) + Cstress + sig_AS_f);
-//                 Ttangent = E*(1 - eps_L*H);
+               Tcsi = (Ccsi*E*Tstrain + Ccsi*sig_AS_f - E*Tstrain + Cstress)/(sig_AS_f - Ccsi*E*eps_L + E*eps_L + Cstress);
+               Tstress = E*(Tstrain + Tcsi*eps_L);
+               double H = (1 - Ccsi)*E/((1 - Ccsi)*(E*eps_L) + Cstress + sig_AS_f);
+               Ttangent = E*(1 - eps_L*H);
          
-//		   }
+	     }
 
-//       }
+       }
 
-//	 else {
+	 else {
 
-//       Tcsi = 1;
-//	 Tstress = E*(Tstrain + Tcsi*eps_L);
-//	 Ttangent = E;
+           Tcsi = 1;
+	     Tstress = E*(Tstrain + Tcsi*eps_L);
+	     Ttangent = E;
 
-//	 }
-
-//}
-
-	 Tcsi = 0.0;
-	 Tstress = 0.0;
-	 Ttangent = E;
+	 }
 
 }
+
 //-------------------------------------------------------------------------------------
-// THIRD QUADRANT (sigma < 0 and epsilon < 0): transformation S ---> A (unloading)
+// S ---> A  transformation (unloading), sigma < 0 and epsilon < 0
 //-------------------------------------------------------------------------------------
 
 if  ((Tstrain - Cstrain) > 0 && Tstrain < 0.0) { 
 
-//if (Tstrain < 0) {
-//       double eps_SA_s = -sig_SA_s/E - Ccsi*eps_L ;
-//	 double eps_SA_f = -sig_SA_f/E;
+      double eps_SA_s = -sig_SA_s/E - Ccsi*eps_L;
+	double eps_SA_f = -sig_SA_f/E;
 
-//	 if (Tstrain <= eps_SA_s) {
+	if (Tstrain <= eps_SA_s) {
 
-//	 Tcsi = Ccsi;
-//	 Tstress = E*(Tstrain + Tcsi*eps_L);
-//	 Ttangent = E;
-
-//       }
-
-//	 else  if (Tstrain > eps_SA_s && Tstrain <= eps_SA_f) { 
-
-//		   if (Cstress < -sig_SA_s) {
-
-//		   Tcsi = (Ccsi*E*Tstrain + Ccsi*sig_SA_f)/(sig_SA_f - Ccsi*E*eps_L - sig_SA_s);
-//		   Tstress = E*(Tstrain + Tcsi*eps_L);
-//		   double H = -(Ccsi*E)/(-Ccsi*(E*eps_L) - sig_SA_s + sig_SA_f);
-//		   Ttangent = E*(1 - eps_L*H);
-
-//		   }
-
-//		   else {
-
-//		   Tcsi = (Ccsi*E*Tstrain + Ccsi*sig_SA_f)/(sig_SA_f - Ccsi*E*eps_L + Cstress);
-//                 Tstress = E*(Tstrain + Tcsi*eps_L);
-//                 double H = -(Ccsi*E)/(-Ccsi*(E*eps_L) + Cstress + sig_SA_f);
-//                 Ttangent = E*(1 - eps_L*H);
-         
-//		   }
-
-//       }
-
-//	 else {
-
-//       Tcsi = 0;
-//	 Tstress = E*(Tstrain); 
-//	 Ttangent = E;
-
-//	 }
-
-//}
-
-	Tcsi = 0.0;
-	Tstress = 0;
+	Tcsi = Ccsi;
+	Tstress = E*(Tstrain + Tcsi*eps_L);
 	Ttangent = E;
+
+	}
+
+	else if (Tstrain > eps_SA_s && Tstrain <= eps_SA_f) { 
+
+	     if (Cstress < -sig_SA_s) {
+
+		   Tcsi = (Ccsi*E*Tstrain + Ccsi*sig_SA_f)/(sig_SA_f - Ccsi*E*eps_L - sig_SA_s);
+		   Tstress = E*(Tstrain + Tcsi*eps_L);
+		   double H = -(Ccsi*E)/(-Ccsi*(E*eps_L) - sig_SA_s + sig_SA_f);
+		   Ttangent = E*(1 - eps_L*H);
+
+		   }
+
+	     else {
+
+		   Tcsi = (Ccsi*E*Tstrain + Ccsi*sig_SA_f)/(sig_SA_f - Ccsi*E*eps_L + Cstress);
+               Tstress = E*(Tstrain + Tcsi*eps_L);
+               double H = -(Ccsi*E)/(-Ccsi*(E*eps_L) + Cstress + sig_SA_f);
+               Ttangent = E*(1 - eps_L*H);
+         
+		   }
+
+      }
+
+	else {
+
+          Tcsi = 0;
+	    Tstress = E*(Tstrain); 
+	    Ttangent = E;
+
+	}
 
 }
 
+return 0;
 
-
-// Trick to avoid not-converged solutions. This line enforce the tangent to assume
-// the elastic value (i.e. Young's modulus)
-
-//if (Tstrain*Cstrain < 1.0e-9) {
-
-//	 Ttangent = E;
-
-//}
-
-//if (Tstrain*Cstrain <= 0) {
-
-
-//	 else {
-
-//   Cstrain = -1.0e-9;
-
-//	 }
-
-//Ccsi = 0;
-//Tstress = 0;
-//Cstress = E*Cstrain; 
-//Tcsi = 0;
-//Ttangent = 0;
-
-//}
-
-//////////////////////////////////////////// 
-
-  return 0;
 }
 
 double 
@@ -424,6 +303,7 @@ SMAMaterial::revertToLastCommit(void)
 int 
 SMAMaterial::revertToStart(void)
 {
+  
   // Reset committed history variables
 
   Cstrain = 0.0 ;
@@ -448,11 +328,13 @@ SMAMaterial::getCopy(void)
   SMAMaterial *theCopy = new SMAMaterial(this->getTag(), E, eps_L, sig_AS_s, sig_AS_f, sig_SA_s, sig_SA_f);
 
   // Copy committed history variables
+ 
   theCopy->Cstrain = Cstrain;
   theCopy->Cstress = Cstress;
   theCopy->Ccsi = Ccsi;
 
   // Copy trial state variables
+ 
   theCopy->Tcsi = Tcsi;
   theCopy->Tstrain = Tstrain;
   theCopy->Tstress = Tstress;
