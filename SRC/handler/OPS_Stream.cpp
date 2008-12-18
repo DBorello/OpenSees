4ca774e43ca1d40745634718f1b35d1a5a20ac66
fmk <fmk@0eff8f49-d3cf-49fd-9ff1-3c36910be21e>
2008-12-18 22:49:50 +0000

    fmk - moving virtual operators to .cpp file
    git-svn-id: svn://peera.berkeley.edu/usr/local/svn/OpenSees/trunk@3760 0eff8f49-d3cf-49fd-9ff1-3c36910be21e

--- SRC/handler/OPS_Stream.cpp
+++ SRC/handler/OPS_Stream.cpp
@@ -18,8 +18,8 @@
 **                                                                    **
 ** ****************************************************************** */
                                                                         
-// $Revision: 1.2 $
-// $Date: 2006-08-03 23:23:20 $
+// $Revision: 1.3 $
+// $Date: 2008-12-18 22:49:50 $
 // $Source: /usr/local/cvs/OpenSees/SRC/handler/OPS_Stream.cpp,v $
 
 #include <OPS_Stream.h>
@@ -33,3 +33,48 @@ OPS_Stream::~OPS_Stream()
 {
 
 }
+
+
+ OPS_Stream& 
+	 OPS_Stream::write(const char *s, int n) {return *this;}
+
+
+ OPS_Stream& 
+	  OPS_Stream::write(const unsigned char *s, int n) {return *this;}
+OPS_Stream& 
+	  OPS_Stream::write(const signed char *s, int n) {return *this;}
+  OPS_Stream& 
+	  OPS_Stream::write(const void *s, int n) {return *this;}
+
+  OPS_Stream& 
+	  OPS_Stream::operator<<(char c) {return *this;}
+  OPS_Stream& 
+	  OPS_Stream::operator<<(unsigned char c) {return *this;}
+  OPS_Stream& 
+	  OPS_Stream::operator<<(signed char c) {return *this;}
+  OPS_Stream& 
+	  OPS_Stream::operator<<(const char *s) {return *this;}
+  OPS_Stream& 
+	  OPS_Stream::operator<<(const unsigned char *s) {return *this;}
+  OPS_Stream& 
+	  OPS_Stream::operator<<(const signed char *s) {return *this;}
+  OPS_Stream& 
+	  OPS_Stream::operator<<(const void *p) {return *this;}
+  OPS_Stream& 
+	  OPS_Stream::operator<<(int n) {return *this;}
+  OPS_Stream& 
+	  OPS_Stream::operator<<(unsigned int n) {return *this;}
+  OPS_Stream& 
+	  OPS_Stream::operator<<(long n) {return *this;}
+  OPS_Stream& 
+	  OPS_Stream::operator<<(unsigned long n) {return *this;}
+  OPS_Stream& 
+	  OPS_Stream::operator<<(short n) {return *this;}
+  OPS_Stream& 
+	  OPS_Stream::operator<<(unsigned short n) {return *this;}
+  OPS_Stream& 
+	  OPS_Stream::operator<<(bool b) {return *this;}
+  OPS_Stream& 
+	  OPS_Stream::operator<<(double n) {return *this;}
+  OPS_Stream& 
+	  OPS_Stream::operator<<(float n) {return *this;}
--- SRC/handler/OPS_Stream.h
+++ SRC/handler/OPS_Stream.h
@@ -18,8 +18,8 @@
 **                                                                    **
 ** ****************************************************************** */
                                                                         
-// $Revision: 1.4 $
-// $Date: 2008-01-16 00:19:01 $
+// $Revision: 1.5 $
+// $Date: 2008-12-18 22:49:50 $
 // $Source: /usr/local/cvs/OpenSees/SRC/handler/OPS_Stream.h,v $
 
 #ifndef _OPS_Stream
@@ -37,11 +37,11 @@ class OPS_Stream:  public MovableObject
   OPS_Stream(int classTag);
   virtual ~OPS_Stream();
 
-  virtual int setFile(const char *fileName, openMode mode = OVERWRITE) {return 0;};
-  virtual int setPrecision(int precision) {return 0;};
-  virtual int setFloatField(floatField) {return 0;};
-  virtual int precision(int precision) {return 0;};
-  virtual int width(int width) {return 0;};
+  virtual int setFile(const char *fileName, openMode mode = OVERWRITE) {return 0;}
+  virtual int setPrecision(int precision) {return 0;}
+  virtual int setFloatField(floatField) {return 0;}
+  virtual int precision(int precision) {return 0;}
+  virtual int width(int width) {return 0;}
 
   // xml stuff
   virtual int tag(const char *) =0;
@@ -53,27 +53,30 @@ class OPS_Stream:  public MovableObject
   virtual int write(Vector &data) =0; 
 
   // regular stuff
-  virtual OPS_Stream& write(const char *s, int n) {return *this;};
-  virtual OPS_Stream& write(const unsigned char *s, int n) {return *this;};
-  virtual OPS_Stream& write(const signed char *s, int n) {return *this;};
-  virtual OPS_Stream& write(const void *s, int n) {return *this;};
+  
+  virtual OPS_Stream& write(const char *s, int n);
+  
+  virtual OPS_Stream& write(const unsigned char *s, int n);
 
-  virtual OPS_Stream& operator<<(char c) {return *this;};
-  virtual OPS_Stream& operator<<(unsigned char c) {return *this;};
-  virtual OPS_Stream& operator<<(signed char c) {return *this;};
-  virtual OPS_Stream& operator<<(const char *s) {return *this;};
-  virtual OPS_Stream& operator<<(const unsigned char *s) {return *this;};
-  virtual OPS_Stream& operator<<(const signed char *s) {return *this;};
-  virtual OPS_Stream& operator<<(const void *p) {return *this;};
-  virtual OPS_Stream& operator<<(int n) {return *this;};
-  virtual OPS_Stream& operator<<(unsigned int n) {return *this;};
-  virtual OPS_Stream& operator<<(long n) {return *this;};
-  virtual OPS_Stream& operator<<(unsigned long n) {return *this;};
-  virtual OPS_Stream& operator<<(short n) {return *this;};
-  virtual OPS_Stream& operator<<(unsigned short n) {return *this;};
-  virtual OPS_Stream& operator<<(bool b) {return *this;};
-  virtual OPS_Stream& operator<<(double n) {return *this;};
-  virtual OPS_Stream& operator<<(float n) {return *this;};
+  virtual OPS_Stream& write(const signed char *s, int n);
+  virtual OPS_Stream& write(const void *s, int n);
+
+  virtual OPS_Stream& operator<<(char c);
+  virtual OPS_Stream& operator<<(unsigned char c);
+  virtual OPS_Stream& operator<<(signed char c);
+  virtual OPS_Stream& operator<<(const char *s);
+  virtual OPS_Stream& operator<<(const unsigned char *s);
+  virtual OPS_Stream& operator<<(const signed char *s);
+  virtual OPS_Stream& operator<<(const void *p);
+  virtual OPS_Stream& operator<<(int n);
+  virtual OPS_Stream& operator<<(unsigned int n);
+  virtual OPS_Stream& operator<<(long n);
+  virtual OPS_Stream& operator<<(unsigned long n);
+  virtual OPS_Stream& operator<<(short n);
+  virtual OPS_Stream& operator<<(unsigned short n);
+  virtual OPS_Stream& operator<<(bool b);
+  virtual OPS_Stream& operator<<(double n);
+  virtual OPS_Stream& operator<<(float n);
 
   int sendSelf(int commitTag, Channel &theChannel) =0;  
   int recvSelf(int commitTag, Channel &theChannel, 
@@ -81,7 +84,7 @@ class OPS_Stream:  public MovableObject
 
 
  private:
-  void indent(void);
+  void indent();
   int numIndent;
 
 
