f61c170bf84622a83eaeb837f76305828848bb1f
fmk <fmk@0eff8f49-d3cf-49fd-9ff1-3c36910be21e>
2010-02-08 21:04:14 +0000

    fmk - adding corotTruss as part of a script test
    git-svn-id: svn://peera.berkeley.edu/usr/local/svn/OpenSees/trunk@4179 0eff8f49-d3cf-49fd-9ff1-3c36910be21e

--- EXAMPLES/ExampleScripts/Example1.1.tcl
+++ EXAMPLES/ExampleScripts/Example1.1.tcl
@@ -22,65 +22,68 @@
 # Start of model generation
 # ------------------------------
 
-# Create ModelBuilder (with two-dimensions and 2 DOF/node)
-model BasicBuilder -ndm 2 -ndf 2
+foreach eleType {truss corotTruss} {
 
-# Create nodes
-# ------------
-
-# Create nodes & add to Domain - command: node nodeId xCrd yCrd
-node 1   0.0  0.0
-node 2 144.0  0.0
-node 3 168.0  0.0
-node 4  72.0 96.0
-
-# Set the boundary conditions - command: fix nodeID xResrnt? yRestrnt?
-fix 1 1 1 
-fix 2 1 1
-fix 3 1 1
-
-# Define materials for truss elements
-# -----------------------------------
-
-# Create Elastic material prototype - command: uniaxialMaterial Elastic matID E
-uniaxialMaterial Elastic 1 3000
-
-# Define elements
-# ---------------
-
-# Create truss elements - command: element truss trussID node1 node2 A matID
-element truss 1 1 4 10.0 1
-element truss 2 2 4 5.0 1
-element truss 3 3 4 5.0 1
-
-
-# Define loads
-# ------------
-
-# Create a Plain load pattern with a linear TimeSeries
-pattern Plain 1 "Linear" {
-
-    # Create the nodal load - command: load nodeID xForce yForce
-    load 4 100 -50
-}
-
-# ------------------------------
-# End of model generation
-# ------------------------------
-
-
-# ------------------------------
-# Start of analysis generation
-# ------------------------------
-
-# Create the system of equation, a SPD using a band storage scheme
-system BandSPD
-
-# Create the DOF numberer, the reverse Cuthill-McKee algorithm
-numberer RCM
-
-# Create the constraint handler, a Plain handler is used as homo constraints
-constraints Plain
+    # Create ModelBuilder (with two-dimensions and 2 DOF/node)
+    model BasicBuilder -ndm 2 -ndf 2
+    wipe
+    # Create nodes
+    # ------------
+    
+    # Create nodes & add to Domain - command: node nodeId xCrd yCrd
+    node 1   0.0  0.0
+    node 2 144.0  0.0
+    node 3 168.0  0.0
+    node 4  72.0 96.0
+    
+    # Set the boundary conditions - command: fix nodeID xResrnt? yRestrnt?
+    fix 1 1 1 
+    fix 2 1 1
+    fix 3 1 1
+    
+    # Define materials for truss elements
+    # -----------------------------------
+    
+    # Create Elastic material prototype - command: uniaxialMaterial Elastic matID E
+    uniaxialMaterial Elastic 1 3000
+    section Elastic 1 3000 5.0 10000
+    
+    # Define elements
+    # ---------------
+    
+    # Create truss elements - command: element truss trussID node1 node2 A matID
+    element $eleType 1 1 4 10.0 1
+    element $eleType 2 2 4 5.0 1
+    element $eleType 3 3 4 1
+    
+    
+    # Define loads
+    # ------------
+    
+    # Create a Plain load pattern with a linear TimeSeries
+    pattern Plain 1 "Linear" {
+	
+	# Create the nodal load - command: load nodeID xForce yForce
+	load 4 100 -50
+    }
+    
+    # ------------------------------
+    # End of model generation
+    # ------------------------------
+    
+    
+    # ------------------------------
+    # Start of analysis generation
+    # ------------------------------
+    
+    # Create the system of equation, a SPD using a band storage scheme
+    system BandSPD
+    
+    # Create the DOF numberer, the reverse Cuthill-McKee algorithm
+    numberer RCM
+    
+    # Create the constraint handler, a Plain handler is used as homo constraints
+    constraints Plain
 
 # Create the integration scheme, the LoadControl scheme using steps of 1.0
 integrator LoadControl 1.0
@@ -119,3 +122,4 @@ analyze 1
 print node 4
 print ele
 
+}
