4346691941cbf1f182c8f2b79468f0e5ac7c9cc7
fmk <fmk@0eff8f49-d3cf-49fd-9ff1-3c36910be21e>
2010-09-11 00:50:53 +0000

    fmk - putting input parsing in file
    git-svn-id: svn://peera.berkeley.edu/usr/local/svn/OpenSees/trunk@4315 0eff8f49-d3cf-49fd-9ff1-3c36910be21e

--- SRC/material/uniaxial/MinMaxMaterial.cpp
+++ SRC/material/uniaxial/MinMaxMaterial.cpp
@@ -18,8 +18,8 @@
 **                                                                    **
 ** ****************************************************************** */
                                                                         
-// $Revision: 1.5 $
-// $Date: 2003-04-02 22:02:42 $
+// $Revision: 1.6 $
+// $Date: 2010-09-11 00:50:53 $
 // $Source: /usr/local/cvs/OpenSees/SRC/material/uniaxial/MinMaxMaterial.cpp,v $
 
 // Written: MHS
@@ -37,6 +37,76 @@
 #include <FEM_ObjectBroker.h>
 
 #include <OPS_Globals.h>
+
+#include <elementAPI.h>
+#define OPS_Export 
+
+OPS_Export void *
+OPS_NewMinMaxMaterial(void)
+{
+  // Pointer to a uniaxial material that will be returned
+  UniaxialMaterial *theMaterial = 0;
+  UniaxialMaterial *theOtherMaterial = 0;
+  double minStrain = -1.0e16;
+  double maxStrain = 1.0e16;
+  int    iData[2];
+
+  int argc = OPS_GetNumRemainingInputArgs();
+  if (argc < 2) {
+    opserr << "WARNING invalid uniaxialMaterial MinMaxMaterial $tag $otherTag <-min $minStrain> <-max $maxStrain>" << endln;
+    return 0;
+  }
+
+  int numData = 2;
+  if (OPS_GetIntInput(&numData, iData) != 0) {
+    opserr << "WARNING invalid uniaxialMaterial MinMaxMaterial $tag $otherTag" << endln;
+    return 0;
+  }
+
+  theOtherMaterial = OPS_GetUniaxialMaterial(iData[1]);
+  if (theOtherMaterial == 0) {
+    opserr << "WARNING invalid otherTag uniaxialMaterial MinMax tag: " << iData[0] << endln;
+    return 0;	
+  }
+
+  argc = OPS_GetNumRemainingInputArgs();  
+  while (argc > 1) {
+    char argvLoc[10];
+    if (OPS_GetString(argvLoc, 10) != 0) {
+      opserr << "WARNING invalid string option uniaxialMaterial MinMax tag: " << iData[0] << endln;
+      return 0;
+    }
+
+    numData = 1;
+
+    if ((strcmp(argvLoc, "-min") == 0) || (strcmp(argvLoc, "-Min") == 0) || (strcmp(argvLoc, "-MIN") == 0)) {
+      if (OPS_GetDouble(&numData, &minStrain) != 0) {      
+	opserr << "WARNING invalid min value  uniaxialMaterial MinMax tag: " << iData[0] << endln;	
+	return 0;
+      }
+    } else if ((strcmp(argvLoc, "-max") == 0) || (strcmp(argvLoc, "-Max") == 0) || (strcmp(argvLoc, "-MAX") == 0)) {
+      if (OPS_GetDouble(&numData, &maxStrain) != 0) {      
+	opserr << "WARNING invalid min value  uniaxialMaterial MinMax tag: " << iData[0] << endln;  
+	return 0;
+      }
+    } else {
+      opserr << "WARNING invalid option:" << argvLoc << " uniaxialMaterial MinMax tag: " << iData[0] << endln;  
+      return 0;
+    }
+    
+    argc = OPS_GetNumRemainingInputArgs();
+  }
+
+  // Parsing was successful, allocate the material
+  theMaterial = new MinMaxMaterial(iData[0], *theOtherMaterial, minStrain, maxStrain);
+
+  if (theMaterial == 0) {
+    opserr << "WARNING could not create uniaxialMaterial of type MinMaxMaterial\n";
+    return 0;
+  }
+
+  return theMaterial;
+}
 
 MinMaxMaterial::MinMaxMaterial(int tag, UniaxialMaterial &material,
 			       double min, double max)
