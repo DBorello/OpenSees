a13bafffafa339b49032bc70830e3237d433cff6
fmk <fmk@0eff8f49-d3cf-49fd-9ff1-3c36910be21e>
2010-11-10 00:02:30 +0000

    fmk - making method virtual - getLocalAxis()
    git-svn-id: svn://peera.berkeley.edu/usr/local/svn/OpenSees/trunk@4361 0eff8f49-d3cf-49fd-9ff1-3c36910be21e

--- SRC/coordTransformation/CrdTransf.h
+++ SRC/coordTransformation/CrdTransf.h
@@ -55,7 +55,7 @@ public:
 
     virtual CrdTransf *getCopy2d(void) {return 0;};
     virtual CrdTransf *getCopy3d(void) {return 0;};
-    int getLocalAxes(Vector &xAxis, Vector &yAxis, Vector &zAxis) {return -1;};
+    virtual int getLocalAxes(Vector &xAxis, Vector &yAxis, Vector &zAxis) {return -1;};
     
     virtual int    initialize(Node *node1Pointer, Node *node2Pointer) = 0;
     virtual int    update(void) = 0;
