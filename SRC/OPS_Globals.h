5e9398b645a39862c2fa6ba884a3ab4344f584da
fmk <fmk@0eff8f49-d3cf-49fd-9ff1-3c36910be21e>
2011-01-03 19:01:50 +0000

    fmk - updating version
    git-svn-id: svn://peera.berkeley.edu/usr/local/svn/OpenSees/trunk@4389 0eff8f49-d3cf-49fd-9ff1-3c36910be21e

--- SRC/OPS_Globals.h
+++ SRC/OPS_Globals.h
@@ -31,7 +31,7 @@
 // Description: This file contains global variables used in OpenSees files.
 // if you change some of the variables, you must recompile ALL the code.
 
-#define OPS_VERSION "2.2.2.c"
+#define OPS_VERSION "2.2.2.d"
 
 #define _USING_OpenSees_STREAMS
 #include <OPS_Stream.h>
