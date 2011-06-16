0e5eb639a1d1d076ead884540185bc8c4aba5bce
mhscott <mhscott@0eff8f49-d3cf-49fd-9ff1-3c36910be21e>
2011-06-16 16:15:48 +0000

    Adding Itpack command
    git-svn-id: svn://peera.berkeley.edu/usr/local/svn/OpenSees/trunk@4560 0eff8f49-d3cf-49fd-9ff1-3c36910be21e

--- SRC/tcl/commands.cpp
+++ SRC/tcl/commands.cpp
@@ -203,6 +203,9 @@ extern TransientIntegrator *OPS_NewGeneralizedAlpha(void);
 
 #include <ConjugateGradientSolver.h>
 
+#include <ItpackLinSOE.h>
+#include <ItpackLinSolver.h>
+
 #include <FullGenLinSOE.h>
 #include <FullGenLinLapackSolver.h>
 
@@ -2522,6 +2525,18 @@ specifySOE(ClientData clientData, Tcl_Interp *interp, int argc, TCL_Char **argv)
     theSOE = new UmfpackGenLinSOE(*theSolver, factLVALUE);      
   }	  
 
+  else if (strcmp(argv[1],"Itpack") == 0) {
+    
+    // now must determine the type of solver to create from rest of args
+    int method = 1;
+    if (argc == 3) {
+      if (Tcl_GetInt(interp, argv[2], &method) != TCL_OK)
+	return TCL_ERROR;
+    }
+    ItpackLinSolver *theSolver = new ItpackLinSolver(method);
+    theSOE = new ItpackLinSOE(*theSolver);      
+  }
+	 
   else if (strcmp(argv[1],"FullGeneral") == 0) {
     // now must determine the type of solver to create from rest of args
     FullGenLinLapackSolver *theSolver = new FullGenLinLapackSolver();
